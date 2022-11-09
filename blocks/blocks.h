4a2a9825aec5128b80e6e4501aa10998d325d0f7
SpicyMileHigh <hugeweeb28@gmail.com>
2022-11-08 17:47:17 -0700

    Fixed cpu percentage error in dwmblocks

--- blocks/blocks.h
+++ blocks/blocks.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
 	{" Bat:", "echo $(cat /sys/class/power_supply/BAT0/capacity)%", 300, 12},
 	{"Bright:", "brightness check", 0, 11},
 	{"Sound:", "amixer sget Master | awk '$6 { print $4 }' | sed -e 's/\\[//' -e 's/]//'",		0,		10},
-	{"CPU:", "echo $(ps -e -o %%cpu | awk '{s+=$1} END {print s}')%", 5, 0},
+	{"CPU:", "echo $(ps -e -o %cpu | awk '{s+=$1} END {print s}')%", 5, 0},
 	{"Mem:", "echo $(free | awk '/^Mem/ { print $2/$3 }' | awk -F . '{ print $1 }')%",	5,		0},
 	{"", "date '+%b %d (%a) %I:%M:%S%p'",					1,		0},
 };
