b2fb36ae8d450b6093407c0ceaeabfe21a1b9870
Cedar-P <usernamesarenoteasy@proton.me>
2023-04-07 23:56:47 -0600

    Added xmonad configuration

---
+++ .config/xmonad/xmonad.hs
@@ -0,0 +1,123 @@
+import XMonad
+import XMonad.Layout.Spacing
+import XMonad.Layout.LayoutModifier
+import XMonad.Layout.Dwindle
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.MultiToggle
+import XMonad.Layout.MultiToggle.Instances
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
+import qualified XMonad.StackSet as W
+import qualified Data.Map as M
+import Graphics.X11.ExtraTypes.XF86
+import System.Exit
+import System.IO
+
+-- XMobar
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+
+myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+mySpacing :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
+mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
+
+myLayout = avoidStruts $
+            tiled ||| Mirror tiled ||| Spiral L XMonad.Layout.Dwindle.CW (3/2) (11/10) ||| Full
+            where
+                tiled = mySpacing 6 $ ResizableTall nmaster delta ratio []
+                nmaster = 1
+                ratio = 1/2
+                delta = 3/100
+
+myStartupHook :: X ()
+myStartupHook = do
+        spawn "/home/some-guy/.config/polybar/launch.sh"
+    
+
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+        -- launch a terminal
+        [ ((modm .|. shiftMask, xK_Return), spawn "urxvt")
+
+        -- application launcher
+        , ((modm, xK_p), spawn "rofi -show run")
+
+        -- Close the focused window
+        , ((modm .|. shiftMask, xK_x), kill)
+
+        -- music control
+        , ((modm .|. shiftMask, xK_j), spawn "mpc toggle")
+        , ((modm .|. shiftMask, xK_h), spawn "mpc prev")
+        , ((modm .|. shiftMask, xK_l), spawn "mpc next")
+        , ((0, xF86XK_AudioRaiseVolume), spawn "snd up")
+        , ((0, xF86XK_AudioLowerVolume), spawn "snd down")
+
+        -- Brightness adjustment
+        , ((modm, xK_F10), spawn "brightness up")
+        , ((modm, xK_F11), spawn "brightness up")
+        --, ((0, xF86XK_MonBrightnessUp), spawn "brightness up")
+        --, ((0, xF86XK_MonBrightnessDown), spawn "brightness down")
+
+        -- Moving around windows
+        , ((modm, xK_j), windows W.focusDown)
+        , ((modm, xK_k), windows W.focusUp)
+        , ((modm, xK_h), sendMessage Shrink)
+        , ((modm, xK_l), sendMessage Expand)
+        , ((modm, xK_Return), windows W.swapMaster)
+
+        -- Exit XMonad
+        , ((modm .|. shiftMask, xK_q), io (exitWith ExitSuccess))
+        -- Restart XMonad
+        , ((modm .|. shiftMask, xK_r), spawn "xmonad --recompile; xmonad --restart")
+
+        , ((modm, xK_1), ((windows $ W.greedyView $ myWorkspaces !! 0)))
+        , ((modm, xK_2), ((windows $ W.greedyView $ myWorkspaces !! 1)))
+        , ((modm, xK_3), ((windows $ W.greedyView $ myWorkspaces !! 2)))
+        , ((modm, xK_4), ((windows $ W.greedyView $ myWorkspaces !! 3)))
+        , ((modm, xK_5), ((windows $ W.greedyView $ myWorkspaces !! 4)))
+        , ((modm, xK_6), ((windows $ W.greedyView $ myWorkspaces !! 5)))
+        , ((modm, xK_7), ((windows $ W.greedyView $ myWorkspaces !! 6)))
+        , ((modm, xK_8), ((windows $ W.greedyView $ myWorkspaces !! 7)))
+        , ((modm, xK_9), ((windows $ W.greedyView $ myWorkspaces !! 8)))
+        , ((modm, xK_0), ((windows $ W.greedyView $ myWorkspaces !! 9)))
+
+        , ((modm .|. shiftMask, xK_1), ((windows $ W.shift $ myWorkspaces !! 0)))
+        , ((modm .|. shiftMask, xK_2), ((windows $ W.shift $ myWorkspaces !! 1)))
+        , ((modm .|. shiftMask, xK_3), ((windows $ W.shift $ myWorkspaces !! 2)))
+        , ((modm .|. shiftMask, xK_4), ((windows $ W.shift $ myWorkspaces !! 3)))
+        , ((modm .|. shiftMask, xK_5), ((windows $ W.shift $ myWorkspaces !! 4)))
+        , ((modm .|. shiftMask, xK_6), ((windows $ W.shift $ myWorkspaces !! 5)))
+        , ((modm .|. shiftMask, xK_7), ((windows $ W.shift $ myWorkspaces !! 6)))
+        , ((modm .|. shiftMask, xK_8), ((windows $ W.shift $ myWorkspaces !! 7)))
+        , ((modm .|. shiftMask, xK_9), ((windows $ W.shift $ myWorkspaces !! 8)))
+        , ((modm .|. shiftMask, xK_0), ((windows $ W.shift $ myWorkspaces !! 9)))
+
+        -- Scroll through the layouts
+        , ((modm, xK_space), sendMessage NextLayout)
+        -- Force a floating window back to tiling
+        , ((modm, xK_t), withFocused $ windows . W.sink)
+
+        -- emacs
+        , ((modm,  xK_e), spawn "emacsclient -a 'emacs' -c -s '/home/some-guy/.config/emacs/server-dir/server'")
+
+        -- manage window spacing
+        , ((modm, xK_minus), decWindowSpacing 2 *> decScreenSpacing 2)
+        , ((modm, xK_equal), incWindowSpacing 2 *> incScreenSpacing 2)
+        ]
+
+main :: IO ()
+main = do
+        --xmonad $ ewmhFullscreen $ ewmh $ xmobarProp $ xmobar $ def {
+        xmonad $ ewmhFullscreen $ ewmh $ xmobarProp def {
+        terminal                = "urxvtc",
+        focusFollowsMouse       = True,
+        clickJustFocuses        = False,
+        borderWidth             = 2,
+        modMask                 = mod4Mask,
+        workspaces              = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+        keys                    = myKeys,
+        --eventHook               = ewmhDesktopsEventHook,
+
+        layoutHook = myLayout,
+        startupHook = myStartupHook
+        }
