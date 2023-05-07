eeffafdfa1882fb72eb30a025c13a74d82d1b0f7
bugger <usernamesarenoteasy@proton.me>
2023-05-07 16:55:49 -0600

    Made universal scrolling script universal for all touchpads

--- .config/xmonad/natScroll.sh
+++ .config/xmonad/natScroll.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-xinput set-int-prop $(xinput list | grep 'Touchpad' | sed -e 's/.*id=//' -e 's/\s.*//') $(xinput list-props 14 | grep -i 'natural scrolling enabled (' | sed -e 's/.*(//' -e 's/).*//') 8 1
+device=$(xinput list | grep 'Touchpad' | sed -e 's/.*id=//' -e 's/\s.*//')
+xinput set-int-prop $device $(xinput list-props $device | grep -i 'natural scrolling enabled (' | sed -e 's/.*(//' -e 's/).*//') 8 1
