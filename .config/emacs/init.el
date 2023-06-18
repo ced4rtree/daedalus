dd66f8858de3e24084624f86ceb3d7ee7b4e0f97
bugger <usernamesarenoteasy@proton.me>
2023-06-18 15:39:20 -0600

    messed around in gnu emacs and improved startup times

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,5 +1,3 @@
-;(setq server-socket-dir (substitute-in-file-name "$HOME/.config/emacs/server-dir"))
-
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
@@ -18,6 +16,15 @@
 
 (setq read-process-output-max (* 1024 1024)) ;; 1 mb
 
+;; Use a hook so the message doesn't get clobbered by other messages.
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Emacs ready in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
+
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
@@ -31,18 +38,43 @@
 (add-hook 'prog-mode-hook (lambda () (rainbow-delimiters-mode 1)))
 (add-hook 'prog-mode-hook (lambda () (rainbow-identifiers-mode 1)))
 
+(set-face-attribute 'default nil
+  :font "JetBrains Mono"
+  :height 120
+  :weight 'medium)
+(set-face-attribute 'variable-pitch nil
+  :font "Ubuntu"
+  :height 120
+  :weight 'medium)
+(set-face-attribute 'fixed-pitch nil
+  :font "JetBrains Mono"
+  :height 120
+  :weight 'medium)
+;; Makes commented text and keywords italics.
+;; This is working in emacsclient but not emacs.
+;; Your font must have an italic face available.
+(set-face-attribute 'font-lock-comment-face nil
+  :slant 'italic)
+(set-face-attribute 'font-lock-keyword-face nil
+  :slant 'italic)
+
+;; This sets the default font on all graphical frames created after restarting Emacs.
+;; Does the same thing as 'set-face-attribute default' above, but emacsclient fonts
+;; are not right unless I also add this method of setting the default font.
+(add-to-list 'default-frame-alist '(font . "JetBrains Mono-11"))
+
 (use-package doom-themes
-  :hook (after-init . (lambda () (load-theme 'doom-one))))
+  :hook (after-init . (lambda () (load-theme 'doom-one t))))
 (use-package doom-modeline
   :ensure t
   :config (doom-modeline-mode 1)
-  (setq doom-modeline-height 35)
-  (setq doom-dark+-blue-modeline t)
-  (setq doom-molokai-brighter-modeline t))
+  (setq doom-modeline-height 35
+        doom-dark+-blue-modeline t))
 
 (setq native-comp-async-report-warnings-errors nil)
 
-(use-package highlight-indent-guides)
+(use-package highlight-indent-guides
+  :defer t)
 (add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
 (setq highlight-indent-guides-method 'character)
 
@@ -91,18 +123,19 @@
 (setq dashboard-set-heading-icons t)
 (setq dashboard-set-file-icons t)
 
-(use-package nyan-mode)
+(use-package nyan-mode :defer t)
 (setq nyan-animate-nyancat t)
 (setq nyan-wavy-trail t)
 (setq nyan-bar-length 80)
 (add-hook 'prog-mode-hook (lambda () (nyan-mode 1)))
 
-(use-package zone)
-(zone-when-idle 120)
+(use-package zone
+  :defer t
+  :config (zone-when-idle 120))
 
-(use-package zone-sl)
-(use-package zone-rainbow)
-(use-package zone-nyan)
+(use-package zone-sl :defer t)
+(use-package zone-rainbow :defer t)
+(use-package zone-nyan :defer t)
 
 (eval-after-load "zone"
   '(unless (memq 'zone-nyan (append zone-programs nil))
@@ -117,18 +150,20 @@
      (setq zone-programs
            (vconcat zone-programs [zone-rainbow]))))
 
-(use-package tree-sitter)
+(use-package tree-sitter :defer t)
 (use-package tree-sitter-langs
+  :defer t
   :after tree-sitter)
 (global-tree-sitter-mode)
 
-(use-package all-the-icons) ; Neat little icons everywhere
-(use-package all-the-icons-dired) ; And in dired too
+(use-package all-the-icons :defer t) ; Neat little icons everywhere
+(use-package all-the-icons-dired :defer t) ; And in dired too
 (use-package beacon ; Neat little light for your cursor
+  :defer t
   :config (beacon-mode 1))
 (global-hl-line-mode 1)
 
-(use-package dired-open)
+(use-package dired-open :defer t)
 (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
 
 (setq dired-open-extensions '(("gif" . "mpv")
@@ -139,42 +174,52 @@
 							  ("mp3" . "mpv")))
 
 (use-package company
+  :defer t
   :ensure t
   :config
   (progn
     (add-hook 'prog-mode-hook 'company-mode)))
 
-(use-package lsp-mode)
-(use-package lsp-haskell)
-(use-package lsp-treemacs)
-(use-package lsp-java)
+(use-package lsp-mode
+  :defer t
+  :hook (prog-mode . #'lsp-deferred)
+  :config
+  (setq lsp-keymap-prefix "C-l"))
+(use-package lsp-haskell
+  :defer t
+  :after lsp-mode)
+(use-package lsp-treemacs
+  :defer t
+  :after lsp-mode)
+(use-package lsp-java
+  :defer t
+  :after lsp-mode)
 
-(setq lsp-keymap-prefix "C-l")
-(add-hook 'prog-mode-hook #'lsp-deferred)
+(use-package flycheck
+  :defer t
+  :config
+  (global-flycheck-mode))
 
-(use-package flycheck)
-(global-flycheck-mode)
+(use-package smartparens
+  :defer t
+  :config
+  (require 'smartparens-config)
+  (smartparens-global-mode))
 
-(use-package smartparens)
-(require 'smartparens-config)
-(smartparens-global-mode)
-
-(use-package vterm)
-(use-package treemacs)
-(use-package sudo-edit)
+(use-package vterm :defer t)
+(use-package treemacs :defer t)
+(use-package sudo-edit :defer t)
 
 (use-package counsel
   :after ivy
-  :config (counsel-mode))
-
-(setq ivy-initial-inputs-alist nil) ; Disable the "^" in interactive counsel commands like M-x
+  :defer t
+  :config
+  (counsel-mode)
+  (setq ivy-initial-inputs-alist nil)) ; Disable the "^" in interactive counsel commands like M-x
 
 (use-package ivy
   :defer 0.1
   :diminish
-  :bind
-  (("C-c C-r" . ivy-resume)
-   ("C-x B" . ivy-switch-buffer-other-window))
   :custom
   (setq ivy-count-format "(%d/%d) ")
   (setq ivy-use-virtual-buffers t)
@@ -184,6 +229,7 @@
 
 (use-package ivy-rich
   :after ivy
+  :defer t
   :custom
   (ivy-virtual-abbreviate 'full
    ivy-rich-switch-buffer-align-virtual-buffer t
@@ -194,8 +240,9 @@
   (ivy-rich-mode 1))
 
 (use-package swiper
-  :after ivy)
-(define-key evil-normal-state-map (kbd "/") 'swiper)
+  :after ivy
+  :defer t)
+(define-key evil-normal-state-map (kbd "/") 'swiper-isearch-backward)
 
 (defalias 'yes-or-no-p 'y-or-n-p) ; Screw typing "yes", all my homies type 'y'
 (setq evil-emacs-state-modes (delq 'ibuffer-mode evil-emacs-state-modes)) ; Lets us use these buffers evilly
@@ -223,7 +270,7 @@
 (define-key evil-motion-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
 (define-key evil-motion-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
 
-(use-package org-bullets)
+(use-package org-bullets :defer t)
 (add-hook 'org-mode-hook 'org-bullets-mode)
 (setq org-hide-leading-stars t)
 
@@ -234,7 +281,8 @@
 	org-src-preserve-indentation t)
 
 (use-package org-auto-tangle
-  :ensure t)
+  :ensure t
+  :defer t)
 (add-hook 'org-mode-hook (lambda () (interactive) (org-auto-tangle-mode 1)))
 
 (use-package org-tempo
@@ -395,28 +443,8 @@ Executes `org-table-copy-down' if in table."
 							 (evil-local-set-key 'insert (kbd "<return>") 'org/shift-return)
 							 (evil-local-set-key 'normal (kbd "<return>") 'org/dwim-at-point)))
 
-; Pretty colors and sizes for org mode
-(defun bugger/org-colors-doom-molokai ()
-(dolist
-	(face
-	 '((org-level-1       1.7 "#fb2874" ultra-bold)
-	   (org-level-2       1.6 "#fd971f" extra-bold)
-	   (org-level-3       1.5 "#9c91e4" bold)
-	   (org-level-4       1.4 "#268bd2" semi-bold)
-	   (org-level-5       1.3 "#e74c3c" normal)
-	   (org-level-6       1.2 "#b6e63e" normal)
-	   (org-level-7       1.1 "#66d9ef" normal)
-	   (org-level-8       1.0 "#e2c770" normal)
-	   (org-table         1.0 "#d4d4d4" normal)
-	   (org-table-header  1.0 "#d4d4d4" normal)
-	   (org-link          1.3 "#9c91e4" normal)))
-	(set-face-attribute (nth 0 face) nil :family 'JetBrainsMono :weight (nth 3 face) :height (nth 1 face) :foreground (nth 2 face)))
-	(set-face-attribute 'org-table nil :family 'JetBrainsMono :weight 'normal :height 1.0 :foreground "#d4d4d4"))
-
 ; thanks dt for this one
-(defun dt/org-colors-doom-one ()
-  "Enable Doom One colors for Org headers."
-  (interactive)
+(with-eval-after-load 'org-faces
   (dolist
       (face
        '((org-level-1 1.7 "#51afef" ultra-bold)
@@ -430,16 +458,14 @@ Executes `org-table-copy-down' if in table."
     (set-face-attribute (nth 0 face) nil :family 'JetBrainsMono :weight (nth 3 face) :height (nth 1 face) :foreground (nth 2 face)))
     (set-face-attribute 'org-table nil :family 'JetBrainsMono :weight 'normal :height 1.0 :foreground "#bfafdf"))
 
-(dt/org-colors-doom-one)
-
 (use-package org-roam
   :ensure t
-  :init
-  (setq org-roam-v2-ack t)
+  :defer t
   :custom
   (org-roam-directory "~/org/notes")
   (org-roam-completion-everywhere t)
   :config
+  (setq org-roam-v2-ack t)
   (org-roam-db-autosync-enable))
 
 (setq org-agenda-files (list "~/org/agenda/todo.org"
@@ -452,12 +478,13 @@ Executes `org-table-copy-down' if in table."
 
 (use-package evil-collection
 	:after evil
+	:defer t
 	:config
 	(setq evil-collection-mode-list '(dashboard dired ibuffer search agenda))
 	(evil-collection-init))
 
 (use-package general
-	:config
+	:init
 	(general-evil-setup t))
 
 (defun bugger/kill-buffer ()
@@ -480,92 +507,103 @@ Executes `org-table-copy-down' if in table."
 	  (org-edit-src-exit)
 	(org-edit-special)))
 
-(nvmap :prefix "SPC b"
-  "i" '(ibuffer :which-key "Ibuffer")
-  "c" '(bugger/kill-buffer :which-key "Close the current buffer")
-  "k" '(bugger/kill-buffer-and-window :which-key "Close the current buffer and window")
-  "b" '(counsel-switch-buffer :which-key "Open a buffer in a new window")
-  "r" '(revert-buffer :which-key "Reload the buffer")
-  "s" '(switch-to-buffer "*scratch*" :which-key "Open the scratch buffer"))
+(defun bugger/reload ()
+  (interactive)
+  (org-babel-tangle (concat user-emacs-directory "config.org"))
+  (byte-compile-file (concat user-emacs-directory "init.el"))
+  (load-file user-init-file)
+  (load-file user-init-file))
+
+(nvmap :prefix "SPC"
+  "b" '(:ignore t :wk "buffer")
+  "b i" '(ibuffer :which-key "Ibuffer")
+  "b c" '(bugger/kill-buffer :which-key "Close the current buffer")
+  "b k" '(bugger/kill-buffer-and-window :which-key "Close the current buffer and window")
+  "b b" '(counsel-switch-buffer :which-key "Open a buffer in a new window")
+  "b r" '(revert-buffer :which-key "Reload the buffer")
+  "b s" '(switch-to-buffer "*scratch*" :which-key "Open the scratch buffer"))
 (define-key evil-normal-state-map (kbd "q") 'bugger/kill-buffer)
 (define-key evil-normal-state-map (kbd "Q") 'bugger/kill-buffer-and-window)
 
-(nvmap :prefix "SPC t"
-  "e" '(bugger/edit-src :which-key "Start/Finish editing a code block")
-  "a" '(org-auto-tangle-mode :which-key "Toggle auto tangle mode")
-  "t" '(org-babel-tangle :which-key "Tangle the current file")
-  "k" '(org-edit-src-abort :which-key "Abort editing a code block"))
-
-(nvmap :prefix "SPC w"
-  "v" '(evil-window-vsplit :which-key "Open a vertical split")
-  "w" '(evil-window-next :which-key "Switch to the next window")
-  "n" '(evil-window-new :which-key "Open a horizontal split")
-  "c" '(evil-window-delete :which-key "Close the current window")
-  "k" #'(lambda ()
+(nvmap :prefix "SPC"
+  "w" '(:ignore t :which-key "window")
+  "w v" '(evil-window-vsplit :which-key "Open a vertical split")
+  "w w" '(evil-window-next :which-key "Switch to the next window")
+  "w n" '(evil-window-new :which-key "Open a horizontal split")
+  "w c" '(evil-window-delete :which-key "Close the current window")
+  "w k" #'(lambda ()
 			(interactive)
 			(when (buffer-modified-p)
 			  (when (y-or-n-p "Buffer modified. Save?")
 				(save-buffer)))
 			(kill-buffer-and-window) :which-key "Close the current buffer and window"))
 
-(nvmap :prefix "SPC d"
-		"d" '(dired :which-key "Open dired")
-		"j" '(dired-jump :which-key "Open dired in the current directory"))
-
-(nvmap :prefix "SPC f"
-  "s" '(save-buffer :which-key "Save file")
-  "r" '(counsel-recentf :which-key "List recent files to open")
-  "u" '(sudo-edit-find-file :which-key "Find file as root")
-  "U" '(sudo-edit :which-key "Edit as root"))
+(nvmap :prefix "SPC"
+  "d" '(:ignore t :which-key "dired")
+  "d" '(dired :which-key "Open dired")
+  "j" '(dired-jump :which-key "Open dired in the current directory"))
 
 (nvmap :prefix "SPC"
-  "." '(find-file :which-key "Open a file"))
+  "." '(find-file :which-key "Open a file")
+  "f" '(:ignore t :which-key "file")
+  "f s" '(save-buffer :which-key "Save file")
+  "f r" '(counsel-recentf :which-key "List recent files to open")
+  "f u" '(sudo-edit-find-file :which-key "Find file as root")
+  "f U" '(sudo-edit :which-key "Edit as root"))
 
-(nvmap :prefix "SPC t"
-  "e" '(lambda ()
-		   (interactive)
-		   (if (org-src-edit-buffer-p)
-			   (org-edit-src-exit)
-			 (org-edit-special)) :which-key "Edia code block")
-  "a" '(org-auto-tangle-mode :which-key "Toggle auto tangle mode")
-  "t" '(org-babel-tangle :which-key "Tangle the current file")
-  "k" '(org-edit-src-abort :which-key "Abort editing a code block"))
+(nvmap :prefix "SPC"
+  "o" '(:ignore t :which-key "org")
+  "o b" '(:ignore t :which-key "babel")
+  "o b e" '(bugger/edit-src :which-key "Edit a code block")
+  "o b a" '(org-auto-tangle-mode :which-key "Toggle auto tangle mode")
+  "o b t" '(org-babel-tangle :which-key "Tangle the current file")
+  "o b k" '(org-edit-src-abort :which-key "Abort editing a code block"))
+
+(nvmap :prefix "SPC"
+  "o" '(:ignore t :which-key "org")
+  "o t" '(:ignore t :which-key "toggle")
+  "o t t" '(org-todo :which-key "Toggle todo"))
 
 (nvmap :prefix "SPC o"
-  "t t" '(org-todo :which-key "Toggle todo"))
+  "r" '(:ignore t :which-key "roam")
+  "r f" '(org-roam-node-find :which-key "Open a note file")
+  "r i" '(org-roam-node-insert :which-key "Insert a roam node")
+  "r r" '(org-roam-buffer-toggle :which-key "Toggle org roam")
+  "r v" '(org-roam-node-visit :which-key "Visit an org node")
+  "r u" '(org-roam-db-sync :which-key "Update roam database")
+  "r c" '(org-capture-finalize :which-key "Finish roam capture")
+  "r a" '(org-capture-kill :which-key "Abort roam capture")
+  "r n" '(org-capture-refile :which-key "Refile roam capture"))
 
-(nvmap :prefix "SPC r"
-  "f" '(org-roam-node-find :which-key "Open a note file")
-  "i" '(org-roam-node-insert :which-key "Insert a roam node")
-  "r" '(org-roam-buffer-toggle :which-key "Toggle org roam")
-  "v" '(org-roam-node-visit :which-key "Visit an org node")
-  "u" '(org-roam-db-sync :which-key "Update roam database")
-  "c" '(org-capture-finalize :which-key "Finish roam capture")
-  "a" '(org-capture-kill :which-key "Abort roam capture")
-  "n" '(org-capture-refile :which-key "Refile roam capture"))
+(nvmap :prefix "SPC o"
+  "a" '(:ignore t :which-key "agenda")
+  "a t" '(org-todo-list :which-key "List TODO entries")
+  "a a" '(org-agenda :which-key "Open org agenda")
+  "a s" '(org-schedule :which-key "Schedule something"))
 
-(nvmap :prefix "SPC a"
-  "t" '(org-todo-list :which-key "List TODO entries")
-  "a" '(org-agenda :which-key "Open org agenda")
-  "s" '(org-schedule :which-key "Schedule something"))
+(nvmap :prefix "SPC"
+  "h" '(:ignore t :which-key "help")
+  "h f" '(describe-function :which-key "Describe a function")
+  "h v" '(describe-variable :which-key "Describe a variable")
+  "h k" '(describe-key :which-key "Describe what a key does")
+  "h r" '(:ignore t :which-key "reload")
+  "h r r" '(bugger/reload :which-key "init file"))
 
-(nvmap :prefix "SPC h"
-  "f" '(describe-function :which-key "Describe a function")
-  "v" '(describe-variable :which-key "Describe a variable")
-  "k" '(describe-key :which-key "Describe what a key does"))
+(nvmap :prefix "SPC"
+  "e" '(:ignore t :which-key "evaluate")
+  "e b" '(eval-buffer (current-buffer) :which-key "Evaluate current buffer")
+  "e r" '(eval-region :which-key "Evaluate region"))
 
-(nvmap :prefix "SPC e"
-  "b" '(eval-buffer (current-buffer) :which-key "Evaluate current buffer")
-  "r" '(eval-region :which-key "Evaluate region"))
-
-(nvmap :prefix "SPC m"
-  "m" '(bookmark-set :which-key "Set a bookmark")
-  "o" '(bookmark-jump :which-key "Jump to a bookmark"))
+(nvmap :prefix "SPC"
+  "b" '(:ignore t :which-key "bookmark")
+  "b m" '(bookmark-set :which-key "Set a bookmark")
+  "b o" '(bookmark-jump :which-key "Jump to a bookmark"))
 
 (global-set-key (kbd "<escape>") 'abort-minibuffers)
 
 (global-set-key (kbd "DEL") 'backward-delete-char)
-(define-key ivy-mode-map (kbd "DEL") 'ivy-backward-delete-char)
+(eval-after-load 'ivy #'(lambda ()
+						  (define-key ivy-mode-map (kbd "DEL") 'ivy-backward-delete-char)))
 (setq c-backspace-function 'backward-delete-char)
 
 (global-set-key (kbd "C-j") #'(lambda ()
@@ -587,33 +625,9 @@ Executes `org-table-copy-down' if in table."
 
 (define-key evil-normal-state-map (kbd "TAB") 'evil-toggle-fold)
 
-; Display some help for forgetting keybindings
 (use-package which-key
 	:ensure t
-	:init
+	:config
 	(which-key-mode))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(doom-dark+ doom-one doom-molokai))
- '(custom-safe-themes
-   '("2721b06afaf1769ef63f942bf3e977f208f517b187f2526f0e57c1bd4a000350" "89d9dc6f4e9a024737fb8840259c5dd0a140fd440f5ed17b596be43a05d62e67" "b99e334a4019a2caa71e1d6445fc346c6f074a05fcbb989800ecbe54474ae1b0" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "be84a2e5c70f991051d4aaf0f049fa11c172e5d784727e0b525565bb1533ec78" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" default))
- '(evil-undo-system 'undo-redo)
- '(org-return-follows-link t)
- '(package-selected-packages
-   '(warning-suppress-types
-	 '((use-package)
-	   (use-package)
-	   (lsp-mode)
-	   (lsp-mode)
-	   (comp))))
- '(warning-suppress-types '((use-package) (use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#1c1e1f" :foreground "#d6d6d4" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "JB" :family "JetBrains Mono")))))
+(setq gc-cons-threshold (* 2 1000 1000))
