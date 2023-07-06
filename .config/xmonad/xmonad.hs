011e412dbcbe85fc2ec292a411369ebcd6b0ced2
bugger <usernamesarenoteasy@proton.me>
2023-07-06 01:03:24 -0600

    played around with xmonad. Added scratchpads + ezconfig keys

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -1,4 +1,6 @@
 import XMonad
+
+-- Layouts
 import XMonad.Layout.Spacing
 import XMonad.Layout.LayoutModifier
 import XMonad.Layout.Dwindle
@@ -15,16 +17,32 @@ import XMonad.Layout.Renamed
 import XMonad.Layout.Simplest
 import XMonad.Layout.SubLayouts
 import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))
+
+-- Hooks
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat)
 import XMonad.Hooks.SetWMName
+import XMonad.Hooks.WindowSwallowing
+import XMonad.Hooks.EwmhDesktops
+
+-- Utils
 import XMonad.Util.SpawnOnce
 import XMonad.Util.Ungrab
+import XMonad.Util.Hacks (windowedFullscreenFixEventHook)
+import XMonad.Util.NamedScratchpad
+import XMonad.Util.WorkspaceCompare
+import XMonad.Util.EZConfig
+
+-- Actions
 import XMonad.Actions.MouseResize
+import XMonad.Actions.NoBorders
+
+-- Various things
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
+import XMonad.ManageHook
 import Data.Monoid
 import Graphics.X11.ExtraTypes.XF86 -- Epic keys
 import System.Exit
@@ -35,13 +53,17 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 
+-- Spacing
 mySpacing :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
 mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
 
+-- Workspaces
 myWorkspaces = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 
+-- Terminal
 myTerminal = "alacritty"
 
+-- Layout definitions
 tall    = renamed [Replace "tall"]
         $ smartBorders
         $ windowNavigation
@@ -49,7 +71,7 @@ tall    = renamed [Replace "tall"]
         $ mySpacing 8
         $ ResizableTall 1 (3/100) (1/2) []
 monocle = renamed [Replace "monocle"]
-        $ smartBorders
+        $ noBorders
         $ windowNavigation
         $ subLayout [] (smartBorders Simplest)
         $ Full
@@ -67,16 +89,42 @@ myLayoutHook = avoidStruts
                                ||| noBorders monocle
                                ||| floats
 
---myLayout = avoidStruts $
-            --toggleLayouts tiled $ noBorders Full
-            --where
-                --tiled = mySpacing 6 $ ResizableTall nmaster delta ratio []
-                --nmaster = 1
-                --ratio = 1/2
-                --delta = 3/100
-
+-- Convenient home variable
 myHome = "/home/some-guy"
 
+-- Scratchpads
+myScratchPads = [ NS "terminal" spawnTerm findTerm manageTerm
+                , NS "ncmpcpp" spawnMus findMus manageMus
+                , NS "calfw" spawnCal findCal manageCal
+                ]
+    where
+      spawnTerm  = myTerminal ++ " -T scratchpad"
+      findTerm   = title =? "scratchpad"
+      manageTerm = customFloating $ W.RationalRect l t w h
+                   where
+                     h = 0.9
+                     w = 0.9
+                     t = 0.075 -- height based
+                     l = 0.05 -- width based
+      spawnMus  = myTerminal ++ " -T music -e ncmpcpp"
+      findMus   = title =? "music"
+      manageMus = customFloating $ W.RationalRect l t w h
+                   where
+                     h = 0.9
+                     w = 0.9
+                     t = 0.075 -- height based
+                     l = 0.05 -- width based
+      spawnCal  = "emacsclient -c -e '(cfw:open-org-calendar)' --title=cal"
+      findCal   = resource =? "cal"
+      manageCal = customFloating $ W.RationalRect l t w h
+                   where
+                     h = 0.9
+                     w = 0.9
+                     t = 0.075 -- height based
+                     l = 0.05 -- width based
+
+
+-- Startup hook
 myStartupHook :: X ()
 myStartupHook = do
   spawnOnce $ concat ["export PATH=${PATH}:", myHome, "/scripts"]
@@ -101,118 +149,126 @@ myStartupHook = do
   -- let java swing apps like intellij work
   setWMName "LG3D"
 
+
+-- Manage hook
 myManageHook :: XMonad.Query (Data.Monoid.Endo WindowSet)
 myManageHook = composeAll
-  [ className =? "confirm"       --> doFloat
-  , className =? "file_progress" --> doFloat
-  , className =? "dialog"        --> doFloat
-  , className =? "download"      --> doFloat
-  , className =? "error"         --> doFloat
-  , className =? "Gimp"          --> doFloat
-  , className =? "notification"  --> doFloat
-  , className =? "splash"        --> doFloat
-  , className =? "toolbar"       --> doFloat
+  [ className =? "confirm"                             --> doFloat
+  , className =? "file_progress"                       --> doFloat
+  , className =? "dialog"                              --> doFloat
+  , className =? "download"                            --> doFloat
+  , className =? "error"                               --> doFloat
+  , className =? "Gimp"                                --> doFloat
+  , className =? "notification"                        --> doFloat
+  , className =? "splash"                              --> doFloat
+  , className =? "toolbar"                             --> doFloat
   , (className =? "firefox" <&&> resource =? "Dialog") --> doFloat
-  , isFullscreen --> doFullFloat
-  ] <+> manageDocks
+  , isFullscreen                                       --> doFullFloat
+  ] <+> manageDocks <+> namedScratchpadManageHook myScratchPads
 
-myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+-- Keymap definition
+myKeys =
         -- launch a terminal
-        [ ((modm .|. shiftMask, xK_Return), windows W.focusMaster >> spawn myTerminal)
+        [ ("M-S-<Return>", windows W.focusMaster >> spawn myTerminal)
 
         -- application launcher
-        , ((modm, xK_p), spawn "rofi -show drun -terminal alacritty" >> spawn "mpv /opt/sounds/menu-01.mp3")
+        , ("M-p", spawn (concat ["rofi -show drun -terminal", myTerminal]) >> spawn "mpv /opt/sounds/menu-01.mp3")
 
         -- Close the focused window
-        , ((modm .|. shiftMask, xK_x), kill)
+        , ("M-S-x", kill)
 
         -- music control
-        , ((modm .|. shiftMask, xK_j), spawn "mpc toggle")
-        , ((modm .|. shiftMask, xK_h), spawn "mpc prev")
-        , ((modm .|. shiftMask, xK_l), spawn "mpc next")
-        , ((0, xF86XK_AudioRaiseVolume), spawn "~/scripts/snd up")
-        , ((0, xF86XK_AudioLowerVolume), spawn "~/scripts/snd down")
+        , ("M-S-j", spawn "mpc toggle")
+        , ("M-S-h", spawn "mpc prev")
+        , ("M-S-l", spawn "mpc next")
+        , ("<XF86AudioRaiseVolume>", spawn "~/scripts/snd up")
+        , ("<XF86AudioLowerVolume>", spawn "~/scripts/snd down")
 
         -- Brightness adjustment
-        -- , ((modm, xK_F10), spawn "~/scripts/brightness down")
-        -- , ((modm, xK_F11), spawn "~/scripts/brightness up")
-        , ((shiftMask, xF86XK_MonBrightnessUp), spawn "brightness up")
-        , ((shiftMask, xF86XK_MonBrightnessDown), spawn "brightness down")
+        , ("S-<XF86MonBrightnessUp>", spawn "brightness up")
+        , ("S-<XF86MonBrightnessDown>", spawn "brightness down")
 
-        , ((0, xF86XK_MonBrightnessUp), spawn "real-brightness up")
-        , ((0, xF86XK_MonBrightnessDown), spawn "real-brightness down")
+        , ("<XF86MonBrightnessUp>", spawn "real-brightness up")
+        , ("<XF86MonBrightnessDown>", spawn "real-brightness down")
+
+        -- Scratchpads
+        , ("M-s <Return>", namedScratchpadAction myScratchPads "terminal")
+        , ("M-s m", namedScratchpadAction myScratchPads "ncmpcpp")
+        , ("M-s c", namedScratchpadAction myScratchPads "calfw")
 
         -- Change the background
-        , ((modm, xK_w), spawn "feh --bg-scale --randomize ~/wallpapers")
+        , ("M-w", spawn "feh --bg-scale --randomize ~/wallpapers")
 
         -- Screenshot
-        , ((modm, xK_s), unGrab *> spawn "scrot 'pictures/%Y-%m-%d-%H-%M.png' -s")
+        , ("M-S-s s", unGrab *> spawn (concat ["import ", myHome, "/Pictures/$(date +%Y%m%d_%H\\h%m\\m%Ss).png"]))
+        , ("M-S-s S-s", unGrab *> spawn (concat ["import -window root ", myHome, "/Pictures/$(date +%Y%m%d_%H\\h%m\\m%Ss).png"]))
 
         -- Moving around windows
-        , ((modm, xK_j), windows W.focusDown)
-        , ((modm, xK_k), windows W.focusUp)
-        , ((modm, xK_h), sendMessage Shrink)
-        , ((modm, xK_l), sendMessage Expand)
-        , ((modm, xK_Return), windows W.swapMaster)
+        , ("M-j", windows W.focusDown)
+        , ("M-k", windows W.focusUp)
+        , ("M-h", sendMessage Shrink)
+        , ("M-l", sendMessage Expand)
+        , ("M-<Return>", windows W.swapMaster)
 
         -- Exit XMonad
-        , ((modm .|. shiftMask, xK_q), io (exitWith ExitSuccess) >> spawn "mpv /opt/sounds/shutdown-01.mp3" >> spawn "doas shutdown now")
+        , ("M-S-q", io (exitWith ExitSuccess) >> spawn "mpv /opt/sounds/shutdown-01.mp3" >> spawn "doas shutdown now")
         -- Restart XMonad
-        , ((modm .|. shiftMask, xK_r), spawn "xmonad --recompile && xmonad --restart")
+        , ("M-S-r", spawn "xmonad --recompile && xmonad --restart")
 
-        , ((modm, xK_1), ((windows $ W.greedyView $ myWorkspaces !! 0)))
-        , ((modm, xK_2), ((windows $ W.greedyView $ myWorkspaces !! 1)))
-        , ((modm, xK_3), ((windows $ W.greedyView $ myWorkspaces !! 2)))
-        , ((modm, xK_4), ((windows $ W.greedyView $ myWorkspaces !! 3)))
-        , ((modm, xK_5), ((windows $ W.greedyView $ myWorkspaces !! 4)))
-        , ((modm, xK_6), ((windows $ W.greedyView $ myWorkspaces !! 5)))
-        , ((modm, xK_7), ((windows $ W.greedyView $ myWorkspaces !! 6)))
-        , ((modm, xK_8), ((windows $ W.greedyView $ myWorkspaces !! 7)))
-        , ((modm, xK_9), ((windows $ W.greedyView $ myWorkspaces !! 8)))
-        , ((modm, xK_0), ((windows $ W.greedyView $ myWorkspaces !! 9)))
+        , ("M-1", ((windows $ W.greedyView $ myWorkspaces !! 0)))
+        , ("M-2", ((windows $ W.greedyView $ myWorkspaces !! 1)))
+        , ("M-3", ((windows $ W.greedyView $ myWorkspaces !! 2)))
+        , ("M-4", ((windows $ W.greedyView $ myWorkspaces !! 3)))
+        , ("M-5", ((windows $ W.greedyView $ myWorkspaces !! 4)))
+        , ("M-6", ((windows $ W.greedyView $ myWorkspaces !! 5)))
+        , ("M-7", ((windows $ W.greedyView $ myWorkspaces !! 6)))
+        , ("M-8", ((windows $ W.greedyView $ myWorkspaces !! 7)))
+        , ("M-9", ((windows $ W.greedyView $ myWorkspaces !! 8)))
+        , ("M-0", ((windows $ W.greedyView $ myWorkspaces !! 9)))
 
-        , ((modm .|. shiftMask, xK_1), ((windows $ W.shift $ myWorkspaces !! 0)))
-        , ((modm .|. shiftMask, xK_2), ((windows $ W.shift $ myWorkspaces !! 1)))
-        , ((modm .|. shiftMask, xK_3), ((windows $ W.shift $ myWorkspaces !! 2)))
-        , ((modm .|. shiftMask, xK_4), ((windows $ W.shift $ myWorkspaces !! 3)))
-        , ((modm .|. shiftMask, xK_5), ((windows $ W.shift $ myWorkspaces !! 4)))
-        , ((modm .|. shiftMask, xK_6), ((windows $ W.shift $ myWorkspaces !! 5)))
-        , ((modm .|. shiftMask, xK_7), ((windows $ W.shift $ myWorkspaces !! 6)))
-        , ((modm .|. shiftMask, xK_8), ((windows $ W.shift $ myWorkspaces !! 7)))
-        , ((modm .|. shiftMask, xK_9), ((windows $ W.shift $ myWorkspaces !! 8)))
-        , ((modm .|. shiftMask, xK_0), ((windows $ W.shift $ myWorkspaces !! 9)))
+        , ("M-S-1", ((windows $ W.shift $ myWorkspaces !! 0)))
+        , ("M-S-2", ((windows $ W.shift $ myWorkspaces !! 1)))
+        , ("M-S-3", ((windows $ W.shift $ myWorkspaces !! 2)))
+        , ("M-S-4", ((windows $ W.shift $ myWorkspaces !! 3)))
+        , ("M-S-5", ((windows $ W.shift $ myWorkspaces !! 4)))
+        , ("M-S-6", ((windows $ W.shift $ myWorkspaces !! 5)))
+        , ("M-S-7", ((windows $ W.shift $ myWorkspaces !! 6)))
+        , ("M-S-8", ((windows $ W.shift $ myWorkspaces !! 7)))
+        , ("M-S-9", ((windows $ W.shift $ myWorkspaces !! 8)))
+        , ("M-S-0", ((windows $ W.shift $ myWorkspaces !! 9)))
 
         -- Scroll through the layouts
-        , ((modm, xK_space), sendMessage NextLayout)
+        , ("M-<Space>", sendMessage NextLayout)
         -- Force a floating window back to tiling
-        , ((modm, xK_t), withFocused $ windows . W.sink)
+        , ("M-t", withFocused $ windows . W.sink)
         -- Toggle fullscreen
-        , ((modm, xK_m), sendMessage (MT.Toggle NBFULL) >> sendMessage ToggleStruts >> spawn "polybar-msg cmd toggle")
+        , ("M-m", sendMessage (MT.Toggle NBFULL) >> sendMessage ToggleStruts) -- >> spawn "polybar-msg cmd toggle")
         -- Toggle floating
-        , ((modm, xK_f), sendMessage $ T.Toggle "floats")
+        , ("M-f", sendMessage $ T.Toggle "floats")
         -- Toggle bar
-        , ((modm, xK_b), sendMessage ToggleStruts >> spawn "polybar-msg cmd toggle")
+        , ("M-b", sendMessage ToggleStruts >> spawn "polybar-msg cmd toggle")
         -- Spacing can be pretty goofy sometimes, so here's just a keybinding exclusively for struts
-        , ((modm .|. shiftMask, xK_b), sendMessage ToggleStruts)
+        , ("M-S, xK-b", sendMessage ToggleStruts)
 
         -- emacs
-        , ((modm,  xK_e), spawn "emacsclient -a 'emacs' -c")
+        , ("M-e", spawn "emacsclient -a 'emacs' -c")
 
         -- manage window spacing
-        , ((modm, xK_minus), decWindowSpacing 2 *> decScreenSpacing 2)
-        , ((modm, xK_equal), incWindowSpacing 2 *> incScreenSpacing 2)
+        , ("M--", decWindowSpacing 2 *> decScreenSpacing 2)
+        , ("M-=", incWindowSpacing 2 *> incScreenSpacing 2)
         ]
 
+-- Main function
 main :: IO ()
 main = do
-        xmonad $ ewmhFullscreen $ docks . ewmh $ def {
+        xmonad $ ewmhFullscreen $ addEwmhWorkspaceSort (pure (filterOutWs [scratchpadWorkspaceTag])) $ docks . ewmh $ def {
         terminal                  = myTerminal
         , focusFollowsMouse       = True
         , clickJustFocuses        = False
-        , borderWidth             = 1
+        , handleEventHook         = windowedFullscreenFixEventHook <> swallowEventHook (className =? "Alacritty"  <||> className =? "st-256color" <||> className =? "XTerm") (return True)
         , modMask                 = mod4Mask
         , workspaces              = myWorkspaces
-        , keys                    = myKeys
+        , keys                    = \c -> mkKeymap c myKeys
         , layoutHook = myLayoutHook
         , startupHook = myStartupHook
         , manageHook = myManageHook
