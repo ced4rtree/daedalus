ad37439e2a35b837f92e01786dc7a8013b89ba1e
bugger <usernamesarenoteasy@proton.me>
2023-08-22 19:09:41 -0600

    some emacs stuff

--- .config/emacs/autostart.sh
+++ .config/emacs/autostart.sh
@@ -2,11 +2,12 @@
 
 mpv /opt/sounds/startup-01.mp3 &
 xsetroot -cursor_name left_ptr
-~/.config/xmonad/wallpaper.sh
-~/.config/xmonad/natScroll.sh
+wallpaper.sh
+natScroll.sh
 xset r rate 200 65
 setxkbmap -option caps:escape
 picom &
 ~/.config/polybar/launch.sh
+batsignal -M 'dunstify'
 if [ -z "$(pidof mpd)" ]; then mpd; fi # sometimes mpd gets started multiple times, and it sounds like hot garbage getting spoon fed directly into my ears
 wmname "LG3D"
