67ed0231a7d05b9384d2950add259c655721c279
bugger_2 <cedar@jangus.dev>
2024-06-04 23:25:49 -0600

    reworked neovim lsp

--- .config/nvim/lua/core/plugin_config/lsp.lua
+++ .config/nvim/lua/core/plugin_config/lsp.lua
@@ -1,109 +1,66 @@
-local lsp_zero = require('lsp-zero')
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 local lspconfig = require('lspconfig')
 
-lsp_zero.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    lsp_zero.default_keymaps({buffer = bufnr})
-    vim.keymap.set('n', '<M-?>', ':Telescope lsp_references<cr>', {buffer = bufnr})
-    vim.keymap.set('n', '<M-.>', function() vim.lsp.buf.definition() end, {buffer = bufnr})
-    vim.keymap.set('n', '<leader>cr', function() vim.lsp.buf.rename() end, {buffer = bufnr})
-    vim.keymap.set('n', '<leader>ca', function() vim.lsp.buf.code_action() end, {buffer = bufnr})
-    vim.keymap.set('n', '<leader>cu', function() vim.lsp.buf.references() end, {buffer = bufnr})
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
+-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    -- on_attach = my_custom_on_attach,
+    capabilities = capabilities,
+  }
+end
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    ensure_installed = {},
-    handlers = {
-        lsp_zero.default_setup,
-        lua_ls = function()
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            lspconfig.lua_ls.setup(lua_opts)
-        end,
-    }
-})
+-- luasnip setup
+local luasnip = require 'luasnip'
 
-lspconfig.hls.setup{
-    on_attach = on_attach,
-    root_dir = vim.loop.cwd,
-    settings = {
-        rootMarkers = {".git"}
-    }
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+    ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+    -- C-b (back) C-f (forward) for snippet placeholder navigation.
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
 }
 
-lsp_zero.set_sign_icons({
-    error = '✘',
-    warn = '▲',
-    hint = '⚑',
-    info = ''
-})
-
-vim.diagnostic.config({
-    virtual_text = false,
-    severity_sort = true,
-    float = {
-        style = 'minimal',
-        border = 'rounded',
-        source = 'always',
-        header = '',
-        prefix = '',
-    },
-})
-
-local cmp = require('cmp')
-local cmp_action = lsp_zero.cmp_action()
-local cmp_format = lsp_zero.cmp_format()
-
-require('luasnip.loaders.from_vscode').lazy_load()
-
-vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
-
-cmp.setup({
-    formatting = cmp_format,
-    preselect = 'item',
-    completion = {
-        completeopt = 'menu,menuone,noinsert'
-    },
-    window = {
-        documentation = cmp.config.window.bordered(),
-    },
-    sources = {
-        {name = 'path'},
-        {name = 'nvim_lsp'},
-        {name = 'nvim_lua'},
-        {name = 'buffer', keyword_length = 3},
-        {name = 'luasnip', keyword_length = 2},
-    },
-    mapping = cmp.mapping.preset.insert({
-        -- confirm completion item
-        ['<CR>'] = cmp.mapping.confirm({select = false}),
-
-        -- toggle completion menu
-        ['<C-e>'] = cmp_action.toggle_completion(),
-
-        -- tab complete
-        ['<Tab>'] = cmp_action.tab_complete(),
-        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-        -- navigate between snippet placeholder
-        ['<C-d>'] = cmp_action.luasnip_jump_forward(),
-        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-
-        -- scroll documentation window
-        ['<C-f>'] = cmp.mapping.scroll_docs(5),
-        ['<C-u>'] = cmp.mapping.scroll_docs(-5),
-
-        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ['<C-Space>'] = cmp.mapping.complete(),
-    }),
+-- Code actions
+vim.api.nvim_create_autocmd({"CursorHold", "CursorHoldI"}, {
+  group = vim.api.nvim_create_augroup("code_action_sign", { clear = true }),
+  callback = function()
+    require('core.plugin_config.code_action_utils').code_action_listener()
+  end,
 })
--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -25,22 +25,12 @@ return require('packer').startup(function(use)
 
     -- lsp
     use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
+        'neovim/nvim-lspconfig',
         requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
-
             -- Autocompletion
             {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
             {'saadparwaiz1/cmp_luasnip'},
             {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
-
             -- Snippets
             {'L3MON4D3/LuaSnip'},
             {'rafamadriz/friendly-snippets'},
