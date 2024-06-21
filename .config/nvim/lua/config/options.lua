d489b30fd456e1ef51da0eebd608f71f3cdceb4c
bugger <usernamesarenoteasy@proton.me>
2024-06-21 16:54:05 -0600

    updated some stuff in neovim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- tabwidth
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+
+-- I would like autoformatting,
+-- but I can't really configure it the way I want
+vim.g.autoformat = false
