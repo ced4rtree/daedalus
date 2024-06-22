a9470bd87f45f64e03326bf2eff2fb659433bdca
bugger <usernamesarenoteasy@proton.me>
2024-06-22 02:44:01 -0600

    added some doom emacs-like keybindings to neovim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set('n', '<leader>fs', ':w<CR>')
+vim.keymap.set('v', '<leader>fs', ':w<CR>')
+
+vim.keymap.set('n', '<leader>.', '<leader>ff')
+vim.keymap.set('v', '<leader>.', '<leader>ff')
+
+vim.keymap.set('n', '<leader>,', '<leader>fF')
+vim.keymap.set('v', '<leader>,', '<leader>fF')
+
+vim.keymap.set('n', '<leader>wv', '<leader>w|')
+vim.keymap.set('n', '<leader>wn', '<leader>w-')
+vim.keymap.set('v', '<leader>wv', '<leader>w|')
+vim.keymap.set('v', '<leader>wn', '<leader>w-')
