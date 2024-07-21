b12437082ac351ebece8e8cf346aae83ce1d4451
bugger2 <cedar@jangus.dev>
2024-07-21 01:50:22 -0600

    added several features to emacs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -4,7 +4,7 @@
 
 (when (not has-restarted)
   (setq config-dir user-emacs-directory)) ;; to use for some stuff like autostart.sh for example, which I do want in my default user-emacs-directory
-(setq user-emacs-directory "~/.local/share/emacs/")
+(setq user-emacs-directory "~/.cache/emacs/")
 
 (require 'package)
 (setq package-user-dir (concat user-emacs-directory ".local/elpa"))
@@ -12,6 +12,7 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+(require 'use-package-ensure)
 (setq use-package-always-ensure t)
 
 (defvar bugger/use-evil nil "Whether or not to use evil mode and the additional SPC- bindings")
@@ -171,7 +172,159 @@
 
 (global-visual-line-mode 1)
 
+(setq-default tab-width 4
+              c-basic-offset 4
+              c-ts-mode-indent-offset 4
+              c-ts-mode-indent-style 'bsd
+              c-default-style "bsd"
+              indent-tabs-mode t)
+(defvaralias 'c-basic-offset 'tab-width)
+(defvaralias 'c-ts-mode-indent-offset 'tab-width)
+(indent-tabs-mode nil)
+(defun bugger/change-tab-width (WIDTH)
+  "Set the width of a tab to WIDTH in the current buffer"
+  (setq-local tab-width WIDTH
+              c-basic-offset WIDTH
+              c-ts-mode-indent-offset WIDTH
+              java-ts-mode-indent-offset WIDTH))
+;; (add-hook 'java-ts-mode-hook #'(lambda () (interactive) (bugger/change-tab-width 2)))
+
+(electric-pair-mode 1)
+(setq electric-pair-inhibit-predicate
+      `(lambda (c)
+         (if (char-equal c ?<) t (,electric-pair-inhibit-predicate c))))
+
+(add-hook 'compilation-filter-hook 'ansi-color-compilation-filter)
+
+(use-package recentf
+  :ensure t
+  :config
+  ;; remove boilerplate files from recentf list
+  (add-to-list 'recentf-exclude "~/org/agenda/schedule.org")
+  (add-to-list 'recentf-exclude (concat user-emacs-directory "bookmarks")))
+
+(use-package dashboard
+  :ensure page-break-lines
+  :ensure all-the-icons
+  :after recentf
+  :hook (dashboard-mode . (lambda () (interactive) (page-break-lines-mode 1)))
+  :hook (dashboard-mode . (lambda () (interactive) (display-line-numbers-mode -1)))
+  :ensure t
+  :init
+  (setq dashboard-page-separator "
+
+" ;; tell dashboard to use nice looking lines for section seperation
+
+        initial-buffer-choice (lambda () (get-buffer-create "*dashboard*")) ;; tell emacs to use dashboard as startup screen
+        dashboard-items '((recents . 5)
+                          (projects . 5)
+                          (agenda . 5))
+        dashboard-center-content t
+        dashboard-startup-banner (concat config-dir "dash.txt")
+        dashboard-icon-type 'all-the-icons
+        dashboard-set-navigator t
+        dashboard-set-file-icons t
+        dashboard-set-heading-icons t
+        dashboard-display-icons-p t)
+  (advice-add #'dashboard-replace-displayable :override #'identity)
+  :config
+  (dashboard-setup-startup-hook))
+
 (add-to-list 'default-frame-alist '(alpha-background .  90))
+
+(use-package no-littering
+  :ensure t)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(use-package perspective
+  :ensure t
+  :defer nil
+  :commands persp-project-switch
+  :bind (("C-c p k" . persp-kill)
+         ("C-c p p" . persp-project-switch)
+         ("C-c p i" . persp-ibuffer)
+         ("C-c p b" . persp-switch-to-buffer*)
+         ("C-c p ." . persp-switch))
+  :config
+  (setq persp-initial-frame-name "Main")
+  (persp-mode)
+  (defun persp-project-switch ()
+    "Switches to a new project and creates a new perspective for that project"
+    (interactive)
+    (let ((project-dir (project-prompt-project-dir)))
+      (persp-switch (file-name-nondirectory
+                     (directory-file-name
+                      (file-name-directory project-dir))))
+      (project-switch-project project-dir))))
+
+(use-package dired-open
+  :ensure t
+  :after dired
+  :config
+  (setq dired-open-extensions '(("gif" . "mpv --loop")
+                                ("mkv" . "mpv")
+                                ("mp4" . "mpv")
+                                ("mp3" . "foot -e mpv")))
+  :bind (:map dired-mode-map
+              ("f" . dired-open-file)))
+
+(setq backup-directory-alist '((".*" . "~/.cache/emacs/auto-saves")))
+(setq auto-save-file-name-transforms '((".*" "~/.cache/emacs/auto-saves" t)))
+
+(defvar goodbye-message-list (list "Don't leave me!"
+                                   "B-baka! It's not like I liked you anyway..."
+                                   "Thank you for participating in this Aperture Science computer-aided enrichment activity."
+                                   "Emacs, Emacs never changes."
+                                   "Wake up, Mr. Stallman. Wake up and smell the ashes."
+                                   "I don't think you want to do that."
+                                   (concat "I'm sorry " user-login-name ", I'm afraid I can't do that.")
+                                   "In case I don't see ya, good afternoon, good evening, and good night!"
+                                   "Here's looking at you, kid."
+                                   "I do wish we could chat longer, but I'm having an old friend for dinner..."
+                                   "Life moves pretty fast. If you don't stop and look around once and a while you might miss it."
+                                   "So long... partner."
+                                   "I'll be right here..."
+                                   "I think this just might be my masterpiece."
+                                   "Where we go from there is a choice I leave to you."
+                                   "Daisy, Daisy, give me your answer do."
+                                   "Leaving? Emacs? Are you well?")
+  "A list of messages used as prompts for the user when quiting emacs")
+(defun quit-emacs (&rest STUFF)
+  (interactive)
+  (y-or-n-p (concat (nth (random (length goodbye-message-list))
+                         goodbye-message-list)
+                    " Really quit emacs?")))
+(global-set-key (kbd "C-x C-c") (lambda ()
+                                  (interactive)
+                                  (when (quit-emacs)
+                                    (save-buffers-kill-terminal))))
+
+(use-package drag-stuff
+  :ensure t
+  :init (drag-stuff-global-mode t)
+  :bind (("M-p" . drag-stuff-up)
+         ("M-n" . drag-stuff-down)))
+
+(use-package yasnippet
+  :ensure t
+  :ensure yasnippet-snippets
+  :defer t
+  :hook (prog-mode . (lambda () (interactive) (yas-minor-mode 1)))
+  :init
+  (setq yas-snippet-dirs (list
+                          (concat user-emacs-directory ".local/elpa/yasnippet-snippets-20230815.820/snippets/")
+                          (concat config-dir "snippets/"))))
+
+(use-package expand-region
+  :bind ("C-=" . er/expand-region))
+
+(add-hook 'prog-mode-hook #'hs-minor-mode)
+
+(use-package esh-autosuggest
+  :hook (eshell-mode . esh-autosuggest-mode))
+
+(zone-when-idle 300)
 
 (use-package org-tempo
   :ensure nil)
@@ -291,149 +444,30 @@
   (tab-always-indent t)
   :hook (prog-mode . corfu-mode))
 
+(use-package diff-hl
+  :hook ((prog-mode . diff-hl-mode)
+         (dired-mode . diff-hl-dired-mode)
+         (magit-pre-refresh-hook . diff-hl-magit-pre-refresh)
+         (magit-post-refresh-hook . diff-hl-magit-post-refresh)))
+
+(use-package blamer
+  :bind (("s-i" . blamer-show-commit-info)
+         ("C-c i" . blamer-show-posframe-commit-info))
+  :defer 20
+  :custom
+  (blamer-idle-time 0.3)
+  (blamer-min-offset 70)
+  :custom-face
+  (blamer-face ((t :foreground "#7a88cf"
+                    :background nil
+                    :height 140
+                    :italic t)))
+  :config
+  (global-blamer-mode 1))
+
 (use-package eldoc-box
   :defer t
   :hook (eglot-managed-mode . eldoc-box-hover-at-point-mode))
-
-(use-package recentf
-  :ensure t
-  :config
-  ;; remove boilerplate files from recentf list
-  (add-to-list 'recentf-exclude "~/org/agenda/schedule.org")
-  (add-to-list 'recentf-exclude (concat user-emacs-directory "bookmarks")))
-
-(use-package dashboard
-  :ensure page-break-lines
-  :ensure all-the-icons
-  :after recentf
-  :hook (dashboard-mode . (lambda () (interactive) (page-break-lines-mode 1)))
-  :hook (dashboard-mode . (lambda () (interactive) (display-line-numbers-mode -1)))
-  :ensure t
-  :init
-  (setq dashboard-page-separator "
-
-" ;; tell dashboard to use nice looking lines for section seperation
-
-        initial-buffer-choice (lambda () (get-buffer-create "*dashboard*")) ;; tell emacs to use dashboard as startup screen
-        dashboard-items '((recents . 5)
-                          (projects . 5)
-                          (agenda . 5))
-        dashboard-center-content t
-        dashboard-startup-banner (concat config-dir "dash.txt")
-        dashboard-icon-type 'all-the-icons
-        dashboard-set-navigator t
-        dashboard-set-file-icons t
-        dashboard-set-heading-icons t
-        dashboard-display-icons-p t)
-  (advice-add #'dashboard-replace-displayable :override #'identity)
-  :config
-  (dashboard-setup-startup-hook))
-
-(use-package no-littering
-  :ensure t)
-
-(add-hook 'compilation-filter-hook 'ansi-color-compilation-filter)
-
-(electric-pair-mode 1)
-(setq electric-pair-inhibit-predicate
-      `(lambda (c)
-         (if (char-equal c ?<) t (,electric-pair-inhibit-predicate c))))
-
-(setq-default tab-width 4
-              c-basic-offset 4
-              c-ts-mode-indent-offset 4
-              c-ts-mode-indent-style 'bsd
-              c-default-style "bsd"
-              indent-tabs-mode t)
-(defvaralias 'c-basic-offset 'tab-width)
-(defvaralias 'c-ts-mode-indent-offset 'tab-width)
-(indent-tabs-mode nil)
-(defun bugger/change-tab-width (WIDTH)
-  "Set the width of a tab to WIDTH in the current buffer"
-  (setq-local tab-width WIDTH
-              c-basic-offset WIDTH
-              c-ts-mode-indent-offset WIDTH
-              java-ts-mode-indent-offset WIDTH))
-;; (add-hook 'java-ts-mode-hook #'(lambda () (interactive) (bugger/change-tab-width 2)))
-
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-(use-package perspective
-  :ensure t
-  :defer nil
-  :bind (("C-c p k" . persp-kill)
-         ("C-c p p" . persp-switch)
-         ("C-c p i" . persp-ibuffer)
-         ("C-c p b" . persp-switch-to-buffer*))
-  :config
-  (setq persp-initial-frame-name "Main")
-  (persp-mode)
-  (defun persp-project-switch ()
-    "Switches to a new project and creates a new perspective for that project"
-    (interactive)
-    (let ((project-dir (project-prompt-project-dir)))
-      (persp-switch (file-name-nondirectory
-                     (directory-file-name
-                      (file-name-directory project-dir))))
-      (project-switch-project project-dir))))
-
-(use-package dired-open
-  :ensure t
-  :after dired
-  :config
-  (setq dired-open-extensions '(("gif" . "mpv --loop")
-                                ("mkv" . "mpv")
-                                ("mp4" . "mpv")
-                                ("mp3" . "foot -e mpv")))
-  :bind (:map dired-mode-map
-              ("f" . dired-open-file)))
-
-(setq backup-directory-alist '((".*" . "~/.cache/emacs/auto-saves")))
-(setq auto-save-file-name-transforms '((".*" "~/.cache/emacs/auto-saves" t)))
-
-(defvar goodbye-message-list (list "Don't leave me!"
-                                   "B-baka! It's not like I liked you anyway..."
-                                   "Thank you for participating in this Aperture Science computer-aided enrichment activity."
-                                   "Emacs, Emacs never changes."
-                                   "Wake up, Mr. Stallman. Wake up and smell the ashes."
-                                   "I don't think you want to do that."
-                                   (concat "I'm sorry " user-login-name ", I'm afraid I can't do that.")
-                                   "In case I don't see ya, good afternoon, good evening, and good night!"
-                                   "Here's looking at you, kid."
-                                   "I do wish we could chat longer, but I'm having an old friend for dinner..."
-                                   "Life moves pretty fast. If you don't stop and look around once and a while you might miss it."
-                                   "So long... partner."
-                                   "I'll be right here..."
-                                   "I think this just might be my masterpiece."
-                                   "Where we go from there is a choice I leave to you."
-                                   "Daisy, Daisy, give me your answer do."
-                                   "Leaving? Emacs? Are you well?")
-  "A list of messages used as prompts for the user when quiting emacs")
-(defun quit-emacs (&rest STUFF)
-  (interactive)
-  (y-or-n-p (concat (nth (random (length goodbye-message-list))
-                         goodbye-message-list)
-                    " Really quit emacs?")))
-(global-set-key (kbd "C-x C-c") (lambda ()
-                                  (interactive)
-                                  (when (quit-emacs)
-                                    (save-buffers-kill-terminal))))
-
-(use-package drag-stuff
-  :ensure t
-  :init (drag-stuff-global-mode t)
-  :bind (("M-p" . drag-stuff-up)
-         ("M-n" . drag-stuff-down)))
-
-(use-package yasnippet
-  :ensure t
-  :ensure yasnippet-snippets
-  :defer t
-  :hook (prog-mode . (lambda () (interactive) (yas-minor-mode 1)))
-  :init
-  (setq yas-snippet-dirs (list
-                          (concat user-emacs-directory ".local/elpa/yasnippet-snippets-20230815.820/snippets/")
-                          (concat config-dir "snippets/"))))
 
 (use-package calfw
   :ensure t
