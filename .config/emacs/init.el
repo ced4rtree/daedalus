27a2fc6141f23617c3a02439bff4214cb4ba5937
bugger2 <usernamesarenoteasy@proton.me>
2024-11-16 23:08:42 -0700

    [emacs] autostart lsp in java

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -218,7 +218,8 @@
   			  (interactive)
   			  (when (or (string= major-mode "java-mode")
   				    (string= major-mode "java-ts-mode"))
-  			    (eglot-java-mode t)))))
+  			    (eglot-java-mode t))))
+  :hook (java-mode . eglot-java-mode))
 
 (use-package magit
   :defer t)
@@ -403,19 +404,3 @@ If TEXT does not have a range, return nil."
   (drag-stuff-define-keys))
 
 (setq gc-cons-threshold (* 2 1024 1024))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
- '(package-vc-selected-packages
-   '((org-modern-indent :url "https://github.com/jdtsmith/org-modern-indent")
-     (indent-bars :url "https://github.com/jdtsmith/indent-bars")))
- '(safe-local-variable-values '((org-latex-default-packages-alist))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
