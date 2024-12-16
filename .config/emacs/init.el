b95e4487f362668cb9e184579d5d456e078fb2b4
bugger2 <usernamesarenoteasy@proton.me>
2024-12-16 13:34:33 -0700

    [emacs] added colors to compilation buffer

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -240,3 +240,14 @@
 
 ;; magit
 (use-package magit :defer t)
+
+;; ansi colors in compilation buffer
+;; (http://endlessparentheses.com/ansi-colors-in-the-compilation-buffer-output.html)
+(require 'ansi-color)
+(defun endless/colorize-compilation ()
+  "Colorize from `compilation-filter-start' to `point'."
+  (let ((inhibit-read-only t))
+    (ansi-color-apply-on-region
+     compilation-filter-start (point))))
+
+(add-hook 'compilation-filter-hook #'endless/colorize-compilation)
