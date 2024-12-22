63f356e1e47a7b23b6fbd0f831ec4962c90b45d6
ced4rtree <usernamesarenoteasy@proton.me>
2024-12-21 22:17:25 -0700

    [emacs] moved custom-file

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -328,17 +328,5 @@
                                        "<:<" ";;;"))
   (global-ligature-mode t))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(cmake-mode eglot-java elcord emms haskell-mode indent-bars ligature
-                magit markdown-mode nix-mode rust-mode web-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; move custom nonsense to a different file
+(setq custom-file (concat user-emacs-directory "custom.el"))
