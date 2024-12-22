8cc9cf3ef9e056057df576c84ff69b19590dd7d7
ced4rtree <usernamesarenoteasy@proton.me>
2024-12-21 23:30:57 -0700

    [emacs] adjusted tab-bar aesthetics

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -95,6 +95,9 @@
         nil)))
 ;; tab bar mode
 (tab-bar-mode t)
+(setq tab-bar-show 1
+      tab-bar-close-button-show nil
+      tab-bar-format '(tab-bar-format-tabs tab-bar-separator))
 
 (defun cedar/tab-name (tab)
   "Returns the name of the specified tab as a string"
