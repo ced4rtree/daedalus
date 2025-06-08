7cd8140c0a28c322e668e65ae8d94d360ffeffbe
ced4rtree <usernamesarenoteasy@proton.me>
2025-06-08 01:33:33 -0600

    [nixos] enable zsh syntax highlighting

--- files/.config/nixos/system/zsh.nix
+++ files/.config/nixos/system/zsh.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }: {
   programs.zsh.enable = true;
+  programs.zsh.syntaxHighlighting.enable = true;
   environment.shells = [ pkgs.zsh ];
   users.users.cedar.shell = pkgs.zsh;
 
