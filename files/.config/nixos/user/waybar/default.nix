5e3496ff1b192c5d56d7780ec8d816d0764cc6f6
ced4rtree <usernamesarenoteasy@proton.me>
2025-06-08 18:17:39 -0600

    hehe forgor

---
+++ files/.config/nixos/user/waybar/default.nix
@@ -0,0 +1,109 @@
+{ config, pkgs, lib, ... }: {
+  programs.waybar = {
+    enable = true;
+    style =
+      (builtins.readFile ./style.css)
+      + (builtins.readFile ./frappe.css);
+
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        height = 10;
+
+        modules-left = ["hyprland/workspaces" "hyprland/window"];
+        modules-right = [
+          "idle_inhibitor"
+          "custom/weather"
+          "pulseaudio"
+          "network"
+          "cpu"
+          "memory"
+          "temperature"
+          "backlight"
+          "battery"
+          "clock"
+          "tray"
+        ];
+
+        "tray" = {
+          icon-size = 21;
+          spacing = 10;
+        };
+
+        "clock" = {
+          format = "{:%B %d, %Y %I:%M%p}";
+          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+        };
+
+        "cpu" = {
+          format = "{usage}%  ";
+          tooltip = false;
+        };
+
+        "memory" = {
+          format = "{}%  ";
+        };
+
+        "temperature" = {
+          hwmon-path = "/sys/class/hwmon/hwmon6/temp1_input";
+          critical-threshold = 80;
+          format = "{temperatureC}°C {icon}";
+          format-icons = ["" "" ""];
+        };
+
+        "backlight" = {
+          format = "{percent}% {icon} ";
+          format-icons = ["" "" "" "" "" "" "" "" ""];
+        };
+
+        "battery" = {
+          states = {
+            warning = 30;
+            critical = 15;
+          };
+          format = "{capacity}% {icon} ";
+          format-charging = "{capacity}% 󱐋";
+          format-plugged = "{capacity}% ";
+          format-alt = "{time} {icon}";
+          format-icons = ["" "" "" "" ""];
+        };
+
+        "network" = {
+          format-wifi = "{signalStrength}%  ";
+          format-ethernet = "{ipaddr}/{cidr}";
+          tooltip-format = "{essid}";
+          format-linked = "{ifname} (No IP)";
+          format-disconnected = "Disconnected ⚠";
+          format-alt = "{ifname}: {ipaddr}/{cidr}";
+        };
+
+        "pulseaudio" = {
+          format = "{volume}% {icon}";
+          format-bluetooth = "{volume}% {icon}";
+          format-bluetooth-muted = " {icon}";
+          format-muted = "";
+          format-source = "{volume}%";
+          format-source-muted = "";
+          format-icons = {
+            headphone = " ";
+            hands-free = " ";
+            headset = " ";
+            phone = "";
+            portable = "";
+            car = "";
+            default = ["" "" " "];
+          };
+          on-click = "pavucontrol";
+        };
+
+        "custom/weather" = {
+          exec = "nix-shell -p python3 --run ~/.config/waybar/scripts/weather.py";
+          restart-interval = 300;
+          return-type = "json";
+          on-click = "xdg-open https://weather.com/en-IN/weather/today/l/$location_id";
+        };
+      };
+    };
+  };
+}
