9f31c06101e20afc7fff8ebc61b24c9beb03d57a
ced4rtree <usernamesarenoteasy@proton.me>
2025-06-09 00:20:40 -0600

    fix emacs? idk why broken?

--- user/emacs/default.nix
+++ user/emacs/default.nix
@@ -1,6 +1,11 @@
 { lib, config, pkgs, ... }: let
   emacs = pkgs.emacs30-pgtk;
 in {
+  home.packages = with pkgs; [
+    emacsPackages.mu4e
+    mu
+  ];
+  
   services.emacs = {
     enable = true;
     package = emacs;
@@ -11,11 +16,8 @@ in {
     enable = true;
     package = emacs;
     extraConfig = (builtins.readFile ./init.el);
-
-    extraPackages = epkgs: [
-      pkgs.mu
-      epkgs.mu4e
-      pkgs.emacsPackages.mu4e
-    ];
   };
+
+  home.file.".config/emacs/init.el".source = ./init.el;
+  home.file.".config/emacs/early-init.el".source = ./early-init.el;
 }
