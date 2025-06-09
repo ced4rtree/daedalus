6d0cdd7e32b2275d05433408c3a92f0d996eccad
ced4rtree <usernamesarenoteasy@proton.me>
2025-06-08 19:15:13 -0600

    [nixos] got waybar under control! :partying_face:

--- files/.config/nixos/user/waybar/weather.nix
+++ files/.config/nixos/user/waybar/weather.nix
@@ -1,14 +1,15 @@
 { config, pkgs, lib, ... }: {
-  weather-script = pkgs.stdenv.mkDerivation {
-    name = "weather.py";
-    propagatedBuildInputs = [
-      (pkgs.python3.withPackages (pythonPackages: python312Packages; [
-        pyquery
-      ]))
-    ];
-    dontUnpack = true;
-    installPhase = "install -Dm755 ${./weather.py} $out/bin/weather.py";
-  };
-
-  home.packages = [ weather-script ];
+  home.packages = let
+    weather-script = pkgs.stdenv.mkDerivation {
+      name = "weather.py";
+      propagatedBuildInputs = [
+        (pkgs.python3.withPackages (pythonPackages: with pkgs.python312Packages; [
+          pyquery
+        ]))
+      ];
+      dontUnpack = true;
+      installPhase = "install -Dm755 ${./weather.py} $out/bin/weather.py";
+    };
+  in
+    [ weather-script ];
 }
