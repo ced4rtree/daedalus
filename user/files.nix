24e1364d31d7dcb5f49c53a47157b68834ea4be0
ced4rtree <usernamesarenoteasy@proton.me>
2025-06-15 22:15:16 -0600

    move .face.icon to .face

--- user/files.nix
+++ user/files.nix
@@ -1,5 +1,5 @@
 { config, lib, pkgs, ... }: {
   home.file.".local/share/wallpapers/wallpaper.jpg".source = ../files/.local/share/wallpapers/wallpaper.jpg;
-  home.file.".face.icon".source = ../files/.face.icon;
+  home.file.".face".source = ../files/.face;
   home.file.".gnupg/gpg.conf".source = ../files/.gnupg/gpg.conf;
 }
