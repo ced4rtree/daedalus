df49d9c401a054a0eafe839e5ce148799d3226c1
ced4rtree <usernamesarenoteasy@proton.me>
2025-06-17 22:26:10 -0600

    [mpd] got things working nicely

--- user/mpd.nix
+++ user/mpd.nix
@@ -1,3 +1,17 @@
-{ config, lib, pkgs, ... }: {
-  services.mpd.enable = true;
+{ config, lib, pkgs, inputs, ... }: {
+  services.mpd = {
+    enable = true;
+    extraConfig = ''
+      audio_output {
+        type "pipewire"
+        name "Pipewire Output"
+      }
+    '';
+  };
+
+  services.mpdris2 = {
+    enable = true;
+    multimediaKeys = true;
+    notifications = true;
+  };
 }
