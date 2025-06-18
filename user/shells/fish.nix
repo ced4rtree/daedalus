4c8c9c6dfb248c9092384fb15144e716ce0312c5
ced4rtree <usernamesarenoteasy@proton.me>
2025-06-18 14:28:55 -0600

    [fish] get everything comfy

--- user/shells/fish.nix
+++ user/shells/fish.nix
@@ -1,12 +1,20 @@
 { config, pkgs, lib, ... }: let
   user = "cedar";
   homeManagerSessionVars = "/etc/profiles/per-user/${user}/etc/profile.d/hm-session-vars.sh";
+  catppuccin-fish = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "fish";
+    rev = "0ce27b518e8ead555dec34dd8be3df5bd75cff8e";
+    hash = "sha256-Dc/zdxfzAUM5NX8PxzfljRbYvO9f9syuLO8yBr+R3qg=";
+  };
 in {
   home.packages = with pkgs; [
     krabby
   ];
 
-  programs.zsh = {
+  xdg.configFile."fish/themes/Catppuccin Mocha.theme".source = "${catppuccin-fish}/themes/Catppuccin Mocha.theme";
+
+  programs.fish = {
     enable = true;
 
     shellInit = ''
@@ -15,11 +23,13 @@ in {
         source ~/.conda/etc/profile.d/conda.sh
       end
 
+      # pokemon
       krabby random | awk 'NR>1 { print $0 }'
-    '';
 
-    sessionVariables = {
-      PATH = "$PATH:$HOME/.local/bin/";
-    };
+      set -U fish_user_paths $HOME/.local/bin/ $fish_user_paths
+      set -U fish_greeting
+
+      fish_config theme choose "Catppuccin Mocha"
+    '';
   };
 }
