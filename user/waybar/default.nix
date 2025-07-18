1dc167221eaf47abc8bef5370f9fc4ab9ddcfd2e
ced4rtree <usernamesarenoteasy@proton.me>
2025-07-17 22:22:42 -0600

    [waybar] use niri workspaces

--- user/waybar/default.nix
+++ user/waybar/default.nix
@@ -24,7 +24,7 @@
         position = "bottom";
         height = 10;
 
-        modules-left = ["hyprland/workspaces" "hyprland/window"];
+        modules-left = ["niri/workspaces" "niri/window"];
         modules-right = [
           "idle_inhibitor"
           "mpris"
