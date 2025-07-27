65f11b383d1514b75464e7fad3726807977d503f
ced4rtree <usernamesarenoteasy@proton.me>
2025-07-26 23:45:35 -0600

    [grub] reformat + add arch linux boot entry

--- system/grub.nix
+++ system/grub.nix
@@ -4,31 +4,41 @@
     systemd-boot.enable = false;
     efi.canTouchEfiVariables = true;
 
-    grub.enable = true;
-    grub.efiSupport = true;
-    grub.device = "nodev";
+    grub = {
+      enable = true;
+      efiSupport = true;
+      devices = [ "nodev" ];
 
-    grub.theme = pkgs.stdenv.mkDerivation {
-      name = "CyberRe";
-      pname = "CyberRe";
-      src = pkgs.fetchFromGitHub {
-        owner = "qdwp";
-        repo = "CyberRe";
-        rev = "master";
-        hash = "sha256-FH/KmmANlxfztroBD1rVdA/XxCGxJPT4CeWWmNs+CIg=";
+      theme = pkgs.stdenv.mkDerivation {
+        name = "CyberRe";
+        pname = "CyberRe";
+        src = pkgs.fetchFromGitHub {
+          owner = "qdwp";
+          repo = "CyberRe";
+          rev = "master";
+          hash = "sha256-FH/KmmANlxfztroBD1rVdA/XxCGxJPT4CeWWmNs+CIg=";
+        };
+        installPhase = ''
+          cp -r ./CyberRe $out/
+        '';
       };
-      installPhase = ''
-        cp -r ./CyberRe $out/
+
+      extraEntries = ''
+        menuentry "Arch Linux" {
+          insmod part_gpt
+          insmod fat
+          search --no-floppy --fs-uuid --set=root CA84-2C54
+          linux /vmlinuz-linux-zen root=UUID=f4b92ad9-c69a-4f3d-a305-241bd4b6eb42 rw zswap.enabled=0 rootfstype=ext4 loglevel=3 quiet splash
+          initrd /intel-ucode.img /initramfs-linux-zen.img
+        }
+
+        menuentry "Reboot" {
+          reboot
+        }
+        menuentry "Poweroff" {
+          halt
+        }
       '';
     };
-
-    grub.extraEntries = ''
-      menuentry "Reboot" {
-        reboot
-      }
-      menuentry "Poweroff" {
-        halt
-      }
-    '';
   };
 }
