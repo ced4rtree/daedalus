c87f96be81935585f26cf7cb089e418a2c49196e
ced4rtree <usernamesarenoteasy@proton.me>
2025-08-02 21:27:29 -0600

    [starship] use stylix colors

--- user/shell/starship.nix
+++ user/shell/starship.nix
@@ -2,17 +2,19 @@
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
-    settings = {
+    settings = let
+      colors = config.lib.stylix.colors.withHashtag;
+    in {
       format = 
-        "[](#e78284)"
+        ''[](${colors.red})''
         + "$python"
         + "$username"
-        + "[](bg:#a6d189 fg:#e78284)"
+        + ''[](bg:${colors.green} fg:${colors.red})''
         + "$directory"
-        + "[](fg:#a6d189 bg:#8caaee)"
+        + ''[](fg:${colors.green} bg:${colors.cyan})''
         + "$git_branch"
         + "$git_status"
-        + "[](fg:#8caaee bg:#ca9ee6)"
+        + ''[](fg:${colors.cyan} bg:${colors.blue})''
         + "$c"
         + "$elixir"
         + "$elm"
@@ -23,9 +25,9 @@
         + "$nodejs"
         + "$nim"
         + "$rust"
-        + "[](fg:#ca9ee6 bg:#babbf1)"
+        + ''[](fg:${colors.blue} bg:${colors.magenta})''
         + "$time"
-        + "[ ](fg:#babbf1)"
+        + ''[ ](fg:${colors.magenta})''
         + "$line_break"
         + "$character";
 
@@ -34,13 +36,13 @@
 
       username = {
         show_always = true;
-        style_user = "bg:#e78284 fg:#292c3c";
-        style_root = "bg:#e78284";
+        style_user = "bg:${colors.red} fg:${colors.base00}";
+        style_root = "bg:${colors.red}";
         format = "[$user ]($style)";
       };
 
       directory = {
-        style = "bg:#a6d189 fg:#292c3c";
+        style = "bg:${colors.green} fg:${colors.base00}";
         format = "[ $path ]($style)";
         truncation_length = 3;
         truncation_symbol = "../";
@@ -55,66 +57,66 @@
 
       c = {
         symbol = " ";
-        style = "bg:#ca9ee6 fg:#292c3c";
+        style = "bg:${colors.blue} fg:${colors.base00}";
         format = "[ $symbol ($version) ]($style)";
       };
 
       elixir = {
         symbol = " ";
-        style = "bg:#ca9ee6 fg:#292c3c";
+        style = "bg:${colors.blue} fg:${colors.base00}";
         format = "[ $symbol ($version) ]($style)";
       };
 
       elm = {
         symbol = " ";
-        style = "bg:#ca9ee6 fg:#292c3c";
+        style = "bg:${colors.blue} fg:${colors.base00}";
         format = "[ $symbol ($version) ]($style)";
       };
 
       git_branch = {
         symbol = "";
-        style = "bg:#8caaee fg:#292c3c";
+        style = "bg:${colors.cyan} fg:${colors.base00}";
         format = "[ $symbol $branch ]($style)";
       };
 
       git_status = {
-        style = "bg:#8caaee fg:#292c3c";
+        style = "bg:${colors.cyan} fg:${colors.base00}";
         format = "[$all_status$ahead_behind ]($style)";
       };
 
       golang = {
         symbol = " ";
-        style = "bg:#ca9ee6 fg:#292c3c";
+        style = "bg:${colors.blue} fg:${colors.base00}";
         format = "[ $symbol ($version) ]($style)";
       };
 
       haskell = {
         symbol = " ";
-        style = "bg:#ca9ee6 fg:#292c3c";
+        style = "bg:${colors.blue} fg:${colors.base00}";
         format = "[ $symbol ($version) ]($style)";
       };
 
       java = {
         symbol = " ";
-        style = "bg:#ca9ee6 fg:#292c3c";
+        style = "bg:${colors.blue} fg:${colors.base00}";
         format = "[ $symbol ($version) ]($style)";
       };
 
       julia = {
         symbol = " ";
-        style = "bg:#ca9ee6 fg:#292c3c";
+        style = "bg:${colors.blue} fg:${colors.base00}";
         format = "[ $symbol ($version) ]($style)";
       };
 
       nodejs = {
         symbol = "";
-        style = "bg:#ca9ee6 fg:#292c3c";
+        style = "bg:${colors.blue} fg:${colors.base00}";
         format = "[ $symbol ($version) ]($style)";
       };
 
       nim = {
         symbol = " ";
-        style = "bg:#ca9ee6 fg:#292c3c";
+        style = "bg:${colors.blue} fg:${colors.base00}";
         format = "[ $symbol ($version) ]($style)";
       };
 
@@ -125,14 +127,14 @@
 
       rust = {
         symbol = "";
-        style = "bg:#ca9ee6 fg:#292c3c";
+        style = "bg:${colors.blue} fg:${colors.base00}";
         format = "[ $symbol ($version) ]($style)";
       };
 
       time = {
         disabled = false;
         time_format = "%R"; # Hour:Minute Format
-        style = "bg:#babbf1 fg:#292c3c";
+        style = "bg:${colors.magenta} fg:${colors.base00}";
         format = "[ $time ]($style)";
       };
 
