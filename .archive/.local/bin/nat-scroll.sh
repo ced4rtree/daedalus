b07d247907deda5462ed4d8e85d4322593b1aafe
ced4rtree <usernamesarenoteasy@proton.me>
2025-08-11 10:16:05 -0600

    [archive] delete archive

--- .archive/.local/bin/nat-scroll.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-device=$(xinput list | grep 'Touchpad' | sed -e 's/.*id=//' -e 's/\s.*//')
-xinput set-int-prop $device $(xinput list-props $device | grep -i 'natural scrolling enabled (' | sed -e 's/.*(//' -e 's/).*//') 8 1
