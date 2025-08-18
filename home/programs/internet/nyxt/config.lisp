09a76a663970619a2b21a9c114178acf4a17113c
ced4rtree <usernamesarenoteasy@proton.me>
2025-08-18 17:50:46 -0600

    [nyxt] messin around

--- home/programs/internet/nyxt/config.lisp
+++ home/programs/internet/nyxt/config.lisp
@@ -2,25 +2,37 @@
 
 ;; emacs keybinds
 (define-configuration buffer
-  ((default-modes
-    (pushnew 'nyxt/mode/emacs:emacs-mode %slot-value%))))
+    ((default-modes
+         (pushnew 'nyxt/mode/emacs:emacs-mode %slot-value%))))
 
 ;; emacs keybindings
 (define-configuration web-buffer
-  ((default-modes
-    (pushnew 'nyxt/mode/blocker:blocker-mode %slot-value%))))
+    ((default-modes
+         (pushnew 'nyxt/mode/blocker:blocker-mode %slot-value%))))
 
 ;; reduce tracking
 (define-configuration web-buffer
-  ((default-modes
-    (pushnew 'nyxt/mode/reduce-tracking:reduce-tracking-mode %slot-value%))))
-
-;; dark theme
-(define-configuration browser
-  ((theme theme:+dark-theme+)))
-(define-configuration (web-buffer)
-  ((default-modes (pushnew 'nyxt/mode/style:dark-mode %slot-value%))))
+    ((default-modes
+         (pushnew 'nyxt/mode/reduce-tracking:reduce-tracking-mode %slot-value%))))
 
 ;; don't restore session
 (defmethod customize-instance ((browser browser) &key)
   (setf (slot-value browser 'restore-session-on-startup-p) nil))
+
+(in-package #:nyxt-user)
+
+(defvar *my-search-engines*
+  (list
+   (make-instance 'search-engine
+                  :name "DuckDuckGo"
+                  :shortcut "d"
+                  #+nyxt-4 :control-url #+nyxt-3 :search-url
+                  "https://duckduckgo.com/?q=~a")))
+
+(define-configuration browser
+    ((restore-session-on-startup-p nil)
+     (external-editor-program "emacsclient -r")
+     #+nyxt-4
+     (search-engine-suggestions-p nil)
+     #+nyxt-4
+     (search-engines (append %slot-default% *my-search-engines*))))
