00752ab4a50a3afd8a328da5660909fc26a0c8ad
ced4rtree <usernamesarenoteasy@proton.me>
2025-09-14 22:20:08 -0600

    [niri] use gtk file chooser

--- home/wm/niri/default.nix
+++ home/wm/niri/default.nix
@@ -11,6 +11,7 @@
         base03
       ])
       (builtins.readFile ./config.kdl);
+    xdg.configFile."niri/niri-portals.conf".source = ./niri-portals.conf;
     home.packages = with pkgs; [
       xdg-desktop-portal-gtk
       xwayland-satellite
