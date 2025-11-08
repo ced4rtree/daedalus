8412ffa3eeb86d952df73812347ab049c4e7af08
ced4rtree <usernamesarenoteasy@proton.me>
2025-11-08 14:39:28 -0700

    add sops

--- modules/presets/base/_commonModules.nix
+++ modules/presets/base/_commonModules.nix
@@ -5,4 +5,5 @@
   "desktopPrograms"
   "users"
   "splash"
+  "sops"
 ]
---
+++ modules/sops.nix
@@ -0,0 +1,17 @@
+{ inputs, config, ... }: let
+  sops = {
+    defaultSopsFile = ../secrets/secrets.yaml;
+    defaultSopsFormat = "yaml";
+    age.keyFile = "/home/${config.daedalus.username}/.config/sops/age/keys.txt";
+  };
+in {
+  flake.modules.nixos.sops = {
+    imports = [ inputs.sops-nix.nixosModules.sops ];
+    inherit sops;
+  };
+
+  flake.modules.homeManager.sops = {
+    imports = [ inputs.sops-nix.homeManagerModules.sops ];
+    inherit sops;
+  };
+}
