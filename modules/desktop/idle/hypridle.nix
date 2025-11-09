bc6bd7e1ce7035703970f887ada33d3003f42da3
ced4rtree <usernamesarenoteasy@proton.me>
2025-11-09 10:28:03 -0700

    update lockscreen command for noctalia

--- modules/desktop/idle/hypridle.nix
+++ modules/desktop/idle/hypridle.nix
@@ -8,7 +8,7 @@
       enable = true;
       settings = {
         general = {
-          lock_cmd = "noctalia-shell ipc call lockScreen toggle";
+          lock_cmd = "noctalia-shell ipc call lockScreen lock";
           before_sleep_cmd = "loginctl lock-session";   # lock before suspend.
           after_sleep_cmd = "hyprctl dispatch dpms on"; # to avoid having to press a key twice to turn on the display.
         };
