d9149fbcb837d88811d6d0d5c033407957d9e001
ced4rtree <usernamesarenoteasy@proton.me>
2025-11-27 15:26:39 -0700

    [neovim] refactor nvf into standalone config

--- modules/desktop/editor/neovim.nix
+++ modules/desktop/editor/neovim.nix
@@ -1,21 +1,15 @@
-{
-  flake.modules.homeManager.neovim = { pkgs, inputs, lib, config, ... }: {
-    imports = [
-      inputs.nvf.homeManagerModules.default
-    ];
-
-    stylix.targets.nvf.enable = false;
-
-    programs.nvf = {
-      enable = true;
-
-      settings.vim = {
-        theme = {
-          enable = true;
-          name = "base16-pro-max";
-          transparent = true;
+{ inputs, config, ... }: let
+  # the config identifier gets shadowed later on
+  topConfig = config;
+in {
+  perSystem = { pkgs, config, ... }: let
+    vim = {
+      theme = {
+        enable = true;
+        name = "base16-pro-max";
+        transparent = true;
           base16-colors = {
-            inherit (config.lib.stylix.colors.withHashtag)
+            inherit (topConfig.flake.lib.colors.withHashtag)
             base00
             base01
             base02
@@ -34,198 +28,198 @@
             base0F
             ;
           };
-        };
+      };
 
-        options.scrolloff = 6;
+      options.scrolloff = 6;
 
-        keymaps = [
-          # file bindings
-          {
-            key = "<leader>fs";
-            mode = [ "n" "v" ];
-            action = ":w<CR>";
-            desc = "Save file";
-          }
-          {
-            key = "<leader>.";
-            mode = [ "n" "v" ];
-            action = ":Telescope file_browser path=%:p:h select_buffer=true<CR>";
-            desc = "Find files in cwd [Telescope]";
-          }
-          {
-            key = "<leader>pf";
-            mode = [ "n" "v" ];
-            action = ":Telescope find_files<CR>";
-            desc = "Find file [Telescope]";
-          }
+      keymaps = [
+        # file bindings
+        {
+          key = "<leader>fs";
+          mode = [ "n" "v" ];
+          action = ":w<CR>";
+          desc = "Save file";
+        }
+        {
+          key = "<leader>.";
+          mode = [ "n" "v" ];
+          action = ":Telescope file_browser path=%:p:h select_buffer=true<CR>";
+          desc = "Find files in cwd [Telescope]";
+        }
+        {
+          key = "<leader>pf";
+          mode = [ "n" "v" ];
+          action = ":Telescope find_files<CR>";
+          desc = "Find file [Telescope]";
+        }
 
-          # buffer bindings
-          {
-            key = "<leader>bp";
-            mode = [ "n" "v" ];
-            action = ":bprevious<CR>";
-            desc = "Go to previous buffer";
-          }
-          {
-            key = "<leader>bn";
-            mode = [ "n" "v" ];
-            action = ":bnext<CR>";
-            desc = "Go to next buffer";
-          }
+        # buffer bindings
+        {
+          key = "<leader>bp";
+          mode = [ "n" "v" ];
+          action = ":bprevious<CR>";
+          desc = "Go to previous buffer";
+        }
+        {
+          key = "<leader>bn";
+          mode = [ "n" "v" ];
+          action = ":bnext<CR>";
+          desc = "Go to next buffer";
+        }
 
-          # git
-          {
-            key = "<leader>gg";
-            mode = [ "n" "v" ];
-            action = ":Neogit<CR>";
-            desc = "Git Status [Neogit]";
-          }
+        # git
+        {
+          key = "<leader>gg";
+          mode = [ "n" "v" ];
+          action = ":Neogit<CR>";
+          desc = "Git Status [Neogit]";
+        }
 
-          # window bindings
-          {
-            key = "<leader>ww";
-            mode = [ "n" "v" ];
-            action = "<C-w>w";
-            desc = "Switch window focus";
-          }
-          {
-            key = "<leader>wv";
-            mode = [ "n" "v" ];
-            action = "<C-w>v";
-            desc = "Open vertical split";
-          }
-          {
-            key = "<leader>wn";
-            mode = [ "n" "v" ];
-            action = "<C-w>n";
-            desc = "Open horizontal split";
-          }
-          {
-            key = "<leader>wc";
-            mode = [ "n" "v" ];
-            action = "<C-w>c";
-            desc = "Close current window";
-          }
-          {
-            key = "<leader>w1";
-            mode = [ "n" "v" ];
-            action = "<C-w>o";
-            desc = "Close other windows";
-          }
-          {
-            key = "<leader>w0";
-            mode = [ "n" "v" ];
-            action = "<C-w>o";
-            desc = "Close current window";
-          }
+        # window bindings
+        {
+          key = "<leader>ww";
+          mode = [ "n" "v" ];
+          action = "<C-w>w";
+          desc = "Switch window focus";
+        }
+        {
+          key = "<leader>wv";
+          mode = [ "n" "v" ];
+          action = "<C-w>v";
+          desc = "Open vertical split";
+        }
+        {
+          key = "<leader>wn";
+          mode = [ "n" "v" ];
+          action = "<C-w>n";
+          desc = "Open horizontal split";
+        }
+        {
+          key = "<leader>wc";
+          mode = [ "n" "v" ];
+          action = "<C-w>c";
+          desc = "Close current window";
+        }
+        {
+          key = "<leader>w1";
+          mode = [ "n" "v" ];
+          action = "<C-w>o";
+          desc = "Close other windows";
+        }
+        {
+          key = "<leader>w0";
+          mode = [ "n" "v" ];
+          action = "<C-w>o";
+          desc = "Close current window";
+        }
 
-          # miscellaneous
+        # miscellaneous
+        {
+          key = "<leader>ot";
+          mode = [ "n" "v" ];
+          action = ":ToggleTerm<CR>";
+          desc = "Toggle terminal";
+        }
+        {
+          key = "<leader>oe";
+          mode = [ "n" "v" ];
+          action = ":Explore<CR>";
+          desc = "Open NetRW (File Explorer)";
+        }
+      ];
+
+      autopairs.nvim-autopairs.enable = true;
+      dashboard.alpha = {
+        enable = true;
+        theme = "theta";
+      };
+      debugger.nvim-dap.ui.enable = true;
+      comments.comment-nvim.enable = true;
+      statusline.lualine = {
+        enable = true;
+        theme = "base16";
+      };
+      telescope = {
+        enable = true;
+        extensions = [
           {
-            key = "<leader>ot";
-            mode = [ "n" "v" ];
-            action = ":ToggleTerm<CR>";
-            desc = "Toggle terminal";
-          }
-          {
-            key = "<leader>oe";
-            mode = [ "n" "v" ];
-            action = ":Explore<CR>";
-            desc = "Open NetRW (File Explorer)";
+            name = "file_browser";
+            packages = [ pkgs.vimPlugins.telescope-file-browser-nvim ];
           }
         ];
+      };
 
-        autopairs.nvim-autopairs.enable = true;
-        dashboard.alpha = {
-          enable = true;
-          theme = "theta";
-        };
-        debugger.nvim-dap.ui.enable = true;
-        comments.comment-nvim.enable = true;
-        statusline.lualine = {
-          enable = true;
-          theme = "base16";
-        };
-        telescope = {
-          enable = true;
-          extensions = [
-            {
-              name = "file_browser";
-              packages = [ pkgs.vimPlugins.telescope-file-browser-nvim ];
-            }
-          ];
-        };
+      terminal.toggleterm = {
+        enable = true;
+      };
 
-        terminal.toggleterm = {
-          enable = true;
-        };
+      clipboard = {
+        registers = "unnamedplus";
+        providers.wl-copy.enable = true;
+        enable = true;
+      };
 
-        clipboard = {
-          registers = "unnamedplus";
-          providers.wl-copy.enable = true;
-          enable = true;
-        };
+      treesitter = {
+        enable = true;
+        context.enable = true;
+        fold = true;
+      };
 
-        treesitter = {
-          enable = true;
-          context.enable = true;
-          fold = true;
-        };
+      autocomplete.blink-cmp = {
+        enable = true;
+        friendly-snippets.enable = true;
+      };
 
-        autocomplete.blink-cmp = {
+      git = {
+        neogit.enable = true;
+        gitsigns = {
           enable = true;
-          friendly-snippets.enable = true;
-        };
-
-        git = {
-          neogit.enable = true;
-          gitsigns = {
-            enable = true;
-            setupOpts = {
-              current_line_blame = true;
-            };
+          setupOpts = {
+            current_line_blame = true;
           };
         };
+      };
 
-        languages = {
-          enableDAP = true;
-          enableTreesitter = true;
-          
-          nix.enable = true;
-          rust.enable = true;
-          java.enable = true;
-          python.enable = true;
-          clang.enable = true;
-        };
+      languages = {
+        enableDAP = true;
+        enableTreesitter = true;
 
-        lsp = {
-          enable = true;
-          inlayHints.enable = true;
-        };
+        nix.enable = true;
+        rust.enable = true;
+        java.enable = true;
+        python.enable = true;
+        clang.enable = true;
+      };
 
-        presence.neocord = {
-          enable = true;
-          setupOpts.enable_line_number = true;
-        };
+      lsp = {
+        enable = true;
+        inlayHints.enable = true;
+      };
 
-        ui = {
-          colorful-menu-nvim.enable = true;
-          colorizer.enable = true;
-          noice.enable = true;
-        };
+      presence.neocord = {
+        enable = true;
+        setupOpts.enable_line_number = true;
+      };
 
-        binds.whichKey.enable = true;
+      ui = {
+        colorful-menu-nvim.enable = true;
+        colorizer.enable = true;
+        noice.enable = true;
+      };
 
-        utility = {
-          direnv.enable = true;
-          sleuth.enable = true;
-        };
+      binds.whichKey.enable = true;
 
-        pluginRC.indent-blankline-init = let
-          inherit (inputs.nvf.lib.nvim.lua) toLuaObject;
-          inherit (inputs.nvf.lib.nvim.dag) entryBefore;
-          colors = config.lib.stylix.colors.withHashtag;
-          cfg = config.programs.nvf.settings.vim.visuals.indent-blankline;
-        in entryBefore ["indent-blankline"] ''
+      utility = {
+        direnv.enable = true;
+        sleuth.enable = true;
+      };
+
+      pluginRC.indent-blankline-init = let
+        inherit (inputs.nvf.lib.nvim.lua) toLuaObject;
+        inherit (inputs.nvf.lib.nvim.dag) entryBefore;
+        colors = topConfig.flake.lib.colors.withHashtag;
+        cfg = config.vim.visuals.indent-blankline;
+      in entryBefore ["indent-blankline"] ''
           local hooks = require "ibl.hooks"
           -- create the highlight groups in the highlight setup hook, so they are reset
           -- every time the colorscheme changes
@@ -239,32 +233,42 @@
               vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "${colors.cyan}" })
           end)
         '';
-        visuals = {
-          indent-blankline = {
-            enable = true;
-            setupOpts = {
-              indent = {
-                highlight = [
-                  "RainbowRed"
-                  "RainbowYellow"
-                  "RainbowBlue"
-                  "RainbowOrange"
-                  "RainbowGreen"
-                  "RainbowViolet"
-                  "RainbowCyan"
-                ];
-              };
+      visuals = {
+        indent-blankline = {
+          enable = true;
+          setupOpts = {
+            indent = {
+              highlight = [
+                "RainbowRed"
+                "RainbowYellow"
+                "RainbowBlue"
+                "RainbowOrange"
+                "RainbowGreen"
+                "RainbowViolet"
+                "RainbowCyan"
+              ];
             };
           };
-          fidget-nvim = {
-            enable = true;
-            setupOpts = {
-              progress.display.render_limit = 5;
-            };
-          };
-          rainbow-delimiters.enable = true;
         };
+        fidget-nvim = {
+          enable = true;
+          setupOpts = {
+            progress.display.render_limit = 5;
+          };
+        };
+        rainbow-delimiters.enable = true;
       };
     };
+  in {
+    packages.nvf = (inputs.nvf.lib.neovimConfiguration {
+      inherit pkgs;
+      modules = [ { inherit vim; } ];
+    }).neovim;
+  };
+
+  flake.modules.homeManager.neovim = { pkgs, inputs, lib, config, ... }: let
+    system = pkgs.stdenv.hostPlatform.system;
+  in {
+    home.packages = [ topConfig.flake.packages.${system}.nvf ];
   };
 }
