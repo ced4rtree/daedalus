20551692fc07c5e60fad3cd8e7cd26e4f63fddde
ced4rtree <usernamesarenoteasy@proton.me>
2025-11-30 22:14:51 -0700

    super key broke again :(

--- modules/desktop/wms/hyprland.nix
+++ modules/desktop/wms/hyprland.nix
@@ -1,5 +1,14 @@
 {
-  flake.modules.nixos.hyprland = {
+  flake.modules.nixos.hyprland = { pkgs, ... }: {
+    xdg.portal = {
+      enable = true;
+      wlr.enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal-gnome
+      ];
+    };
+
     programs.hyprland.enable = true;
   };
 
@@ -145,67 +154,67 @@
 
         bind = [
           # APP BINDS
-          "SUPER,return,exec,foot"
-          "SUPER,E,exec,emacsclient -c -a 'emacs'"
-          "SUPERSHIFT,escape,exec,pkill Hyprland"
+          "ALT,return,exec,${config.daedalus.terminalCommand}"
+          "ALT,E,exec,emacsclient -c -a 'emacs'"
+          "ALTSHIFT,escape,exec,pkill Hyprland"
 
           # GENERAL WINDOWS OPERATIONS
-          "SUPER,space,togglefloating,"
-          "SUPER,G,togglegroup,"
-          "SUPER,C,changegroupactive,"
-          "SUPER,R,exec,fuzzel"
-          "SUPER,F,fullscreen,"
-          "SUPER,Escape,exec,noctalia-shell ipc call lockScreen lock"
-          "CTRLSUPER,Escape,exec,noctalia-shell ipc call lockScreen lock; sleep 1; loginctl suspend"
-          "SUPERSHIFT,Q,killactive,"
+          "ALT,space,togglefloating,"
+          "ALT,G,togglegroup,"
+          "ALT,C,changegroupactive,"
+          "ALT,R,exec,fuzzel"
+          "ALT,F,fullscreen,"
+          "ALT,Escape,exec,noctalia-shell ipc call lockScreen lock"
+          "CTRLALT,Escape,exec,noctalia-shell ipc call lockScreen lock; sleep 1; loginctl suspend"
+          "ALTSHIFT,Q,killactive,"
 
           # FOCUS WORKSPACES
-          "SUPER,1,workspace,1"
-          "SUPER,2,workspace,2"
-          "SUPER,3,workspace,3"
-          "SUPER,4,workspace,4"
-          "SUPER,5,workspace,5"
-          "SUPER,6,workspace,6"
-          "SUPER,7,workspace,7"
-          "SUPER,8,workspace,8"
-          "SUPER,9,workspace,9"
-          "SUPER,0,workspace,10"
+          "ALT,1,workspace,1"
+          "ALT,2,workspace,2"
+          "ALT,3,workspace,3"
+          "ALT,4,workspace,4"
+          "ALT,5,workspace,5"
+          "ALT,6,workspace,6"
+          "ALT,7,workspace,7"
+          "ALT,8,workspace,8"
+          "ALT,9,workspace,9"
+          "ALT,0,workspace,10"
 
           # MOVING WINDOWS TO WS
-          "SUPERSHIFT,1,movetoworkspace,1"
-          "SUPERSHIFT,2,movetoworkspace,2"
-          "SUPERSHIFT,3,movetoworkspace,3"
-          "SUPERSHIFT,4,movetoworkspace,4"
-          "SUPERSHIFT,5,movetoworkspace,5"
-          "SUPERSHIFT,6,movetoworkspace,6"
-          "SUPERSHIFT,7,movetoworkspace,7"
-          "SUPERSHIFT,8,movetoworkspace,8"
-          "SUPERSHIFT,9,movetoworkspace,9"
-          "SUPERSHIFT,0,movetoworkspace,10"
-          "SUPERSHIFT,right,movetoworkspace,m+1"
-          "SUPERSHIFT,left,movetoworkspace,m-1"
+          "ALTSHIFT,1,movetoworkspace,1"
+          "ALTSHIFT,2,movetoworkspace,2"
+          "ALTSHIFT,3,movetoworkspace,3"
+          "ALTSHIFT,4,movetoworkspace,4"
+          "ALTSHIFT,5,movetoworkspace,5"
+          "ALTSHIFT,6,movetoworkspace,6"
+          "ALTSHIFT,7,movetoworkspace,7"
+          "ALTSHIFT,8,movetoworkspace,8"
+          "ALTSHIFT,9,movetoworkspace,9"
+          "ALTSHIFT,0,movetoworkspace,10"
+          "ALTSHIFT,right,movetoworkspace,m+1"
+          "ALTSHIFT,left,movetoworkspace,m-1"
 
-          "SUPER,H,movefocus,l"
-          "SUPER,L,movefocus,r"
-          "SUPER,K,movefocus,u"
-          "SUPER,J,movefocus,d"
+          "ALT,H,movefocus,l"
+          "ALT,L,movefocus,r"
+          "ALT,K,movefocus,u"
+          "ALT,J,movefocus,d"
 
-          "SUPERCONTROL,h,swapwindow,l"
-          "SUPERCONTROL,l,swapwindow,r"
-          "SUPERCONTROL,k,swapwindow,u"
-          "SUPERCONTROL,j,swapwindow,d"
+          "ALTCONTROL,h,swapwindow,l"
+          "ALTCONTROL,l,swapwindow,r"
+          "ALTCONTROL,k,swapwindow,u"
+          "ALTCONTROL,j,swapwindow,d"
 
           ",XF86AudioNext,exec,emacsclient -e '(emms-next)'"
-          "SUPER,right,exec,emacsclient -e '(emms-next)'"
+          "ALT,right,exec,emacsclient -e '(emms-next)'"
           ",XF86AudioPrev,exec,emacsclient -e '(emms-previous)'"
-          "SUPER,left,exec,emacsclient -e '(emms-previous)'"
-          "SUPER,down,exec,emacsclient -e '(emms-pause)'"
-          "SUPER,up,exec,emacsclient -e '(emms-pause)'"
+          "ALT,left,exec,emacsclient -e '(emms-previous)'"
+          "ALT,down,exec,emacsclient -e '(emms-pause)'"
+          "ALT,up,exec,emacsclient -e '(emms-pause)'"
           ",XF86AudioPlay,exec,emacsclient -e '(emms-pause)'"
 
           # SCREENSHOTS
-          "SUPER,S,exec,grimshot save area ~/Pictures/screenshot_$(date +%Y%m%d_%H%M%S).png"
-          "SUPERSHIFT,S,exec,grimshot save screen ~/Pictures/screenshot_$(date +%Y%m%d_%H%M%S).png"
+          "ALT,S,exec,grimshot save area ~/Pictures/screenshot_$(date +%Y%m%d_%H%M%S).png"
+          "ALTSHIFT,S,exec,grimshot save screen ~/Pictures/screenshot_$(date +%Y%m%d_%H%M%S).png"
 
           # BRIGHTNESS CONTROL
           ",XF86MonBrightnessUp,exec,brightnessctl set +10%"
@@ -214,16 +223,16 @@
 
         bindm = [
           # MOUSE WINDOW CONTROL
-          "SUPER,mouse:272,movewindow"
-          "SUPER,mouse:273,resizewindow"
+          "ALT,mouse:272,movewindow"
+          "ALT,mouse:273,resizewindow"
         ];
 
         binde = [
           # WINDOW SIZE CONTROL
-          "SUPERSHIFT,H,resizeactive,-20 0"
-          "SUPERSHIFT,L,resizeactive,20 0"
-          "SUPERSHIFT,K,resizeactive,0 -20"
-          "SUPERSHIFT,J,resizeactive,0 20"
+          "ALTSHIFT,H,resizeactive,-20 0"
+          "ALTSHIFT,L,resizeactive,20 0"
+          "ALTSHIFT,K,resizeactive,0 -20"
+          "ALTSHIFT,J,resizeactive,0 20"
 
           # AUDIO CONTROL
           ",XF86AudioRaiseVolume,exec,pactl set-sink-volume @DEFAULT_SINK@ +5%"
