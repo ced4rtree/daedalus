645e0b1e2dc38b8a8f9b549fe4e09fac90f00223
ced4rtree <usernamesarenoteasy@proton.me>
2025-12-09 16:04:21 -0700

    moar blog config

--- modules/presets/server/modules/blog.nix
+++ modules/presets/server/modules/blog.nix
@@ -2,10 +2,56 @@
   flake.modules.nixos.blog = { inputs, pkgs, ... }: {
     services.nginx = {
       enable = true;
-      virtualHosts."cebar.xyz" = {
-        enableACME = true;
-        forceSSL = true;
-        root = "${inputs.cebar-xyz.packages.${pkgs.stdenv.hostPlatform.system}.default}/";
+
+      recommendedGzipSettings = true;
+      recommendedOptimization = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
+
+      # Only allow PFS-enabled ciphers with AES256
+      sslCiphers = "AES256+EECDH:AES256+EDH:!aNULL";
+
+      appendHttpConfig = ''
+        # Add HSTS header with preloading to HTTPS requests.
+        # Adding this header to HTTP requests is discouraged
+        map $scheme $hsts_header {
+            https   "max-age=31536000; includeSubdomains; preload";
+        }
+        add_header Strict-Transport-Security $hsts_header;
+  
+        # Enable CSP for your services.
+        #add_header Content-Security-Policy "script-src 'self'; object-src 'none'; base-uri 'none';" always;
+  
+        # Minimize information leaked to other domains
+        add_header 'Referrer-Policy' 'origin-when-cross-origin';
+  
+        # Disable embedding as a frame
+        add_header X-Frame-Options DENY;
+  
+        # Prevent injection of code in other mime types (XSS Attacks)
+        add_header X-Content-Type-Options nosniff;
+  
+        # This might create errors
+        proxy_cookie_path / "/; secure; HttpOnly; SameSite=strict";
+      '';
+
+      virtualHosts = let
+        base = locations: {
+          inherit locations;
+
+          forceSSL = true;
+          enableACME = true;
+        };
+        proxy = port: base {
+          "/".proxyPass = "http://127.0.0.1:" + toString(port) + "/";
+        };
+      in {
+        "cebar.xyz" = proxy 3000 // {
+          default = true;
+          enableACME = true;
+          forceSSL = true;
+          root = "${inputs.cebar-xyz.packages.${pkgs.stdenv.hostPlatform.system}.default}/";
+        };
       };
     };
 
