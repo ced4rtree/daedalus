9ba7a6d0083bf4c119caabf231822d92f8001af4
ced4rtree <usernamesarenoteasy@proton.me>
2025-12-24 12:40:39 -0700

    use attr for hyprlock config instead of str

--- modules/presets/desktop/modules/lock/hyprlock.nix
+++ modules/presets/desktop/modules/lock/hyprlock.nix
@@ -8,44 +8,42 @@
   };
 
   perSystem = { pkgs, ... }: {
-    packages.hyprlock = let
-      hyprlockConf = with config.flake.lib.stylix.colors; pkgs.writeText "hyprlock.conf" ''
-        general {
-          disable_loading_bar = true
-          grace = 300
-          hide_cursor = true
-          no_fade_in = false
-        }
-
-        background {
-          blur_passes = 1
-          blur_size = 6
-          path = screenshot
-        }
-
-        input-field {
-          size = 300, 40
-          position = 0, 0
-          monitor = 
-          dots_center = true
-          fade_on_empty = false
-          outline_thickness = 5
-          placeholder_text = Password
-          shadow_passes = 2
-
-          # ooh pretty colors
-          outer_color = rgb(${blue})
-          inner_color = rgb(${base00})
-          font_color = rgb(${base05})
-          fail_color = rgb(${base08})
-          check_color = rgb(${base0A})
-        }
-      '';
-    in inputs.wrappers.lib.wrapPackage {
+    packages.hyprlock = inputs.wrappers.lib.wrapPackage {
       inherit pkgs;
       package = pkgs.hyprlock;
 
-      flags."-c" = "${hyprlockConf}";
+      flags."-c" = "${pkgs.writeText "hyprlock.conf" (config.flake.lib.generators.toHyprlang { } {
+        general = {
+          disable_loading_bar = true;
+          grace = 300;
+          hide_cursor = true;
+          no_fade_in = false;
+        };
+
+        background = {
+          blur_passes = 1;
+          blur_size = 6;
+          path = "screenshot";
+        };
+
+        input-field = with config.flake.lib.stylix.colors; {
+          size = "300, 40";
+          position = "0, 0";
+          monitor = "";
+          dots_center = true;
+          fade_on_empty = false;
+          outline_thickness = 5;
+          placeholder_text = "Password";
+          shadow_passes = 2;
+
+          # ooh pretty colors
+          outer_color = "rgb(${blue})";
+          inner_color = "rgb(${base00})";
+          font_color = "rgb(${base05})";
+          fail_color = "rgb(${base08})";
+          check_color = "rgb(${base0A})";
+        };
+      })}";
     };
   };
 }
